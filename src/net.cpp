902e6330f515c49652f147bfa15f6888cec5f099
Philip Kaufmann <phil.kaufmann@t-online.de>
2015-07-03 09:26:51 +0200

    banlist: update set dirty to be more fine grained
    - move the SetBannedSetDirty(false) call from DumpData() into DumpBanlist()
    - ensure we only set false, if the write succeeded

--- src/net.cpp
+++ src/net.cpp
@@ -1483,10 +1483,7 @@ void DumpData()
     DumpAddresses();
 
     if (CNode::BannedSetIsDirty())
-    {
         DumpBanlist();
-        CNode::SetBannedSetDirty(false);
-    }
 }
 
 void static ProcessOneShot()
@@ -2719,16 +2716,16 @@ bool CBanDB::Read(banmap_t& banSet)
 void DumpBanlist()
 {
     int64_t nStart = GetTimeMillis();
-
-    CNode::SweepBanned(); //clean unused entries (if bantime has expired)
+    CNode::SweepBanned(); // clean unused entries (if bantime has expired)
 
     CBanDB bandb;
     banmap_t banmap;
     CNode::GetBanned(banmap);
-    bandb.Write(banmap);
+    if (bandb.Write(banmap))
+        CNode::SetBannedSetDirty(false);
 
     LogPrint("net", "Flushed %d banned node ips/subnets to banlist.dat  %dms\n",
-             banmap.size(), GetTimeMillis() - nStart);
+        banmap.size(), GetTimeMillis() - nStart);
 }
 
 int64_t PoissonNextSend(int64_t nNow, int average_interval_seconds) {
