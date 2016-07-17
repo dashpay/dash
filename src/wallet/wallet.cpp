cd9830aa89dfc4ace20026068ab6ea6ce32b535f
UdjinM6 <UdjinM6@dash.org>
2016-07-17 22:04:10 +0300

    Fix a bug in GetDenominatedCredit

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1720,7 +1720,7 @@ CAmount CWalletTx::GetDenominatedCredit(bool unconfirmed, bool fUseCache) const
     int nDepth = GetDepthInMainChain(false);
     if(nDepth < 0) return 0;
 
-    bool isUnconfirmed = !CheckFinalTx(*this) || (!IsTrusted() && nDepth == 0);
+    bool isUnconfirmed = IsTrusted() && nDepth == 0;
     if(unconfirmed != isUnconfirmed) return 0;
 
     if (fUseCache) {
