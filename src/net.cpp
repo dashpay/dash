b60a8c4b3c88c0b2696e782d9dbb4a4348f35c4c
Cory Fields <cory-nospam-@coryfields.com>
2016-12-31 02:05:19 -0500

    net: record bytes written before notifying the message processor

--- src/net.cpp
+++ src/net.cpp
@@ -1293,9 +1293,9 @@ void CConnman::ThreadSocketHandler()
                             bool notify = false;
                             if (!pnode->ReceiveMsgBytes(pchBuf, nBytes, notify))
                                 pnode->CloseSocketDisconnect();
+                            RecordBytesRecv(nBytes);
                             if(notify)
                                 condMsgProc.notify_one();
-                            RecordBytesRecv(nBytes);
                         }
                         else if (nBytes == 0)
                         {
