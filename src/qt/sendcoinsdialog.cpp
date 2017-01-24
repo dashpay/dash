c90944ecf42c55dec0426a2d4ffac24782dab8bd
Jonas Schnelli <dev@jonasschnelli.ch>
2017-01-24 09:22:56 +0100

    Merge #9588: qt: Use nPowTargetSpacing constant
    fa4d478 qt: Use nPowTargetSpacing constant (MarcoFalke)

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -17,6 +17,7 @@
 #include "walletmodel.h"
 
 #include "base58.h"
+#include "chainparams.h"
 #include "coincontrol.h"
 #include "validation.h" // mempool and minRelayTxFee
 #include "ui_interface.h"
