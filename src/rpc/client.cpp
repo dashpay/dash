3d0d5c22a8851d97739769b4d06c5c3667f6a40c
Sergey <sergey@dash.org>
2017-06-26 04:24:22 -0700

    String version in sentinel ping
    Version format is "x.x.x"

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -125,7 +125,6 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "getaddressdeltas", 0},
     { "getaddressutxos", 0},
     { "getaddressmempool", 0},
-    { "sentinelping", 0},
 };
 
 class CRPCConvertTable
