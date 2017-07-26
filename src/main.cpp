83c32ae9834d511452e95b3f108530b9c513ecb8
negan <WeAreNegan@users.noreply.github.com>
2017-07-26 01:38:58 -0400

    alpha

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
-// Copyright (c) 2014-2017 The Ox Core developers
+// Copyright (c) 2014-2017 The Dash Core developers
+// Copyright (c) 2017 The Ox Coin Dev Team
+
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
