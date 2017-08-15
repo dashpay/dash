9d09881b73c3f7bd2b55b24b8593a9bd60ef06f3
Oleg Girko <ol@infoserver.lv>
2017-08-15 21:07:08 +0100

    Iterate over all nodes in Dash-specific code using AllNodes condition.
    Use AllNodes functional object as newly introduced condition argument for
    ForEachNode() and ForNode() methods of CConnman to iterate over all nodes
    where needed in Dash-specific code.
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -156,7 +156,7 @@ void CActiveMasternode::ManageStateInitial()
     if(!fFoundLocal) {
         bool empty = true;
         // If we have some peers, let's try to find our local address from one of them
-        g_connman->ForEachNodeContinueIf([&fFoundLocal, &empty, this](CNode* pnode) {
+        g_connman->ForEachNodeContinueIf(CConnman::AllNodes, [&fFoundLocal, &empty, this](CNode* pnode) {
             empty = false;
             if (pnode->addr.IsIPv4())
                 fFoundLocal = GetLocal(service, &pnode->addr) && CMasternode::IsValidNetAddr(service);
