0b614fe7aff4a8e15ee2f1bd31c658236c9da669
UdjinM6 <UdjinM6@users.noreply.github.com>
2017-09-07 18:56:20 +0300

    bump CMasternodeMan::SERIALIZATION_VERSION_STRING

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -16,7 +16,7 @@
 /** Masternode manager */
 CMasternodeMan mnodeman;
 
-const std::string CMasternodeMan::SERIALIZATION_VERSION_STRING = "CMasternodeMan-Version-5";
+const std::string CMasternodeMan::SERIALIZATION_VERSION_STRING = "CMasternodeMan-Version-6";
 
 struct CompareLastPaidBlock
 {
