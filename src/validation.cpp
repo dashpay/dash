8a15c8616c41cc0b5173042d477ea6121b394a90
Alexander Block <ablock84@gmail.com>
2017-12-16 11:57:08 +0100

    Implement named devnets
    This allows the creation of multiple independent devnets. Each one is
    identified by a name which is hardened into a "devnet genesis" block,
    which is automatically positioned at height 1. Validation rules will
    ensure that a node from devnet=test1 never be able to accept blocks
    from devnet=test2. This is done by checking the expected devnet genesis
    block.
    
    The genesis block of the devnet is the same as the one from regtest. This
    starts the devnet with a very low difficulty, allowing us to fill up
    needed balances for masternodes very fast.
    
    Also, the devnet name is put into the sub-version of the VERSION message.
    If a node connects to the wrong network, it will immediately be disconnected.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3119,6 +3119,14 @@ bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, bool f
     if (block.GetBlockTime() > GetAdjustedTime() + 2 * 60 * 60)
         return state.Invalid(false, REJECT_INVALID, "time-too-new", "block timestamp too far in the future");
 
+    // Check DevNet
+    if (Params().NetworkIDString() == CBaseChainParams::DEVNET &&
+            block.hashPrevBlock == Params().GetConsensus().hashGenesisBlock &&
+            block.GetHash() != Params().DevNetGenesisBlock().GetHash()) {
+        return state.DoS(100, error("CheckBlockHeader(): wrong devnet genesis"),
+                         REJECT_INVALID, "devnet-genesis");
+    }
+
     return true;
 }
 
@@ -3990,6 +3998,12 @@ bool InitBlockIndex(const CChainParams& chainparams)
 
             if (!AddGenesisBlock(chainparams, chainparams.GenesisBlock(), state))
                 return false;
+
+            if (chainparams.NetworkIDString() == CBaseChainParams::DEVNET) {
+                if (!AddGenesisBlock(chainparams, chainparams.DevNetGenesisBlock(), state))
+                    return false;
+            }
+
             // Force a chainstate write so that when we VerifyDB in a moment, it doesn't check stale data
             return FlushStateToDisk(state, FLUSH_STATE_ALWAYS);
         } catch (const std::runtime_error& e) {
