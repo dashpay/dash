3eaa726861b3ebf1b551f004f8a8e0f26d2f0693
StaterCoin <dev@statercoin.org>
2017-12-16 14:55:18 +0000

    Changes.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,3 +1,4 @@
+
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
@@ -93,7 +94,7 @@ public:
     CMainParams() {
         networkID = CBaseChainParams::MAIN;
         strNetworkID = "main";
-        /** 
+        /**
          * The message start string is designed to be unlikely to occur in normal data.
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
@@ -102,7 +103,8 @@ public:
         pchMessageStart[1] = 0x0c;
         pchMessageStart[2] = 0x6b;
         pchMessageStart[3] = 0xbd;
-        vAlertPubKey = ParseHex("049ec0c34ec67fec7a89c06290297923146d48fa36ac49dbb3c67f716c5214422b4e0199cf8ca670463db697098ee073fbe6a73b17414d5f29315270a1b1f7eae6");
+        vAlertPubKey = 
+ParseHex("049ec0c34ec67fec7a89c06290297923146d48fa36ac49dbb3c67f716c5214422b4e0199cf8ca670463db697098ee073fbe6a73b17414d5f29315270a1b1f7eae6");
         nDefaultPort = 9999;
         bnProofOfWorkLimit = ~uint256(0) >> 20;  // Dash starting difficulty is 1 / 2^12
         nSubsidyHalvingInterval = 210000;
@@ -116,10 +118,11 @@ public:
         /**
          * Build the genesis block. Note that the output of the genesis coinbase cannot
          * be spent as it did not originally exist in the database.
-         * 
+         *
          * CBlock(hash=00000ffd590b14, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=e0028e, nTime=1390095618, nBits=1e0ffff0, nNonce=28917698, vtx=1)
          *   CTransaction(hash=e0028e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
-         *     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d01044c5957697265642030392f4a616e2f3230313420546865204772616e64204578706572696d656e7420476f6573204c6976653a204f76657273746f636b2e636f6d204973204e6f7720416363657074696e6720426974636f696e73)
+         *     CTxIn(COutPoint(000000, -1), coinbase 
+04ffff001d01044c5957697265642030392f4a616e2f3230313420546865204772616e64204578706572696d656e7420476f6573204c6976653a204f76657273746f636b2e636f6d204973204e6f7720416363657074696e6720426974636f696e73)
          *     CTxOut(nValue=50.00000000, scriptPubKey=0xA9037BAC7050C479B121CF)
          *   vMerkleTree: e0028e
          */
@@ -127,9 +130,11 @@ public:
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
-        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned 
+char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].nValue = 20000000 * COIN;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("04390ad6d040a28654163f3793ab604b3f06f695e239d62c36b5c4c6b997762572141adf3588849c701f6dc838c449b8f79f1fdd18edaee3020c7482528211b136") << OP_CHECKSIG;
+        txNew.vout[0].scriptPubKey = CScript() << 
+ParseHex("04390ad6d040a28654163f3793ab604b3f06f695e239d62c36b5c4c6b997762572141adf3588849c701f6dc838c449b8f79f1fdd18edaee3020c7482528211b136") << OP_CHECKSIG;
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
@@ -139,9 +144,9 @@ public:
         genesis.nNonce   = 377681;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x29bcb8cf77c4ccbc46f5e1f9f28d2ba7bbeb938406d37079bdb94efaba4b7f0b"));
-        assert(genesis.hashMerkleRoot == uint256("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
-        
+        assert(hashGenesisBlock == uint256("0xde2a63a58c585d343c1b5ff00035078b72f89505294ac1c7c295bea324a6a2e1"));
+        assert(genesis.hashMerkleRoot == uint256("0xf7fa3227d22347431e66767cd6e038f8da63b734ed03e8d4ec50250f34108062"));
+
         vSeeds.push_back(CDNSSeedData("statercoin.io", "dns.statercoin.io"));
         //vSeeds.push_back(CDNSSeedData("darkcoin.io", "dnsseed.darkcoin.io"));
         //vSeeds.push_back(CDNSSeedData("masternode.io", "dnsseed.masternode.io"));
@@ -167,12 +172,13 @@ public:
 
         nPoolMaxTransactions = 3;
         strSporkKey = "04ec787b0807a3739992efd1b9a514e7697214c94d74e2c9ddbbbf3418d776e7a943f501c3e7d14e5063e6bf01f620a78a1ea3baea63b68c876dad75b0e74fca71";
-        strMasternodePaymentsPubKey = "04ec787b0807a3739992efd1b9a514e7697214c94d74e2c9ddbbbf3418d776e7a943f501c3e7d14e5063e6bf01f620a78a1ea3baea63b68c876dad75b0e74fca71";
+        strMasternodePaymentsPubKey = 
+"04ec787b0807a3739992efd1b9a514e7697214c94d74e2c9ddbbbf3418d776e7a943f501c3e7d14e5063e6bf01f620a78a1ea3baea63b68c876dad75b0e74fca71";
         strDarksendPoolDummyAddress = "Xq19GqFvajRrEdDHYRKGYjTsQfpV5jyipF";
         nStartMasternodePayments = 1513429654; //Wed, 25 Jun 2014 20:36:16 GMT
     }
 
-    const Checkpoints::CCheckpointData& Checkpoints() const 
+    const Checkpoints::CCheckpointData& Checkpoints() const
     {
         return data;
     }
@@ -191,7 +197,8 @@ public:
         pchMessageStart[1] = 0xe2;
         pchMessageStart[2] = 0xca;
         pchMessageStart[3] = 0xff;
-        vAlertPubKey = ParseHex("0477c45547e8eb3a43391d09d990a36c6d1942b66fe198dfdfc9706d652a378f83a3aa6a6473490804869a57ad8b5ab34c8772fb5867e29dca7e9c5d0c08c7a846");
+        vAlertPubKey = 
+ParseHex("0477c45547e8eb3a43391d09d990a36c6d1942b66fe198dfdfc9706d652a378f83a3aa6a6473490804869a57ad8b5ab34c8772fb5867e29dca7e9c5d0c08c7a846");
         nDefaultPort = 19999;
         nEnforceBlockUpgradeMajority = 51;
         nRejectBlockOutdatedMajority = 75;
@@ -205,7 +212,8 @@ public:
         genesis.nNonce = 3045805;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x00000186200bf1d52332e2e3eedbee52826c68ae38fff9f1a697625e60a029b6"));
+
+        assert(hashGenesisBlock == uint256("0x86c50e76fc6ab03a89961c6f43e49c5feac182e972ae572f91d6aac09d522e68"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -236,11 +244,12 @@ public:
 
         nPoolMaxTransactions = 2;
         strSporkKey = "040cfb2c9d8264f0be87b7d35f32ba075c521dc99c8f61bccaa8beb45172661bd0ea2b0a609ffa73c3e6ebb415316fb86496bdf80b9d5535c4b05cee3e8cbbe485";
-        strMasternodePaymentsPubKey = "040cfb2c9d8264f0be87b7d35f32ba075c521dc99c8f61bccaa8beb45172661bd0ea2b0a609ffa73c3e6ebb415316fb86496bdf80b9d5535c4b05cee3e8cbbe485";
+        strMasternodePaymentsPubKey = 
+"040cfb2c9d8264f0be87b7d35f32ba075c521dc99c8f61bccaa8beb45172661bd0ea2b0a609ffa73c3e6ebb415316fb86496bdf80b9d5535c4b05cee3e8cbbe485";
         strDarksendPoolDummyAddress = "y1EZuxhhNMAUofTBEeLqGE1bJrpC2TWRNp";
         nStartMasternodePayments = 1420837558; //Fri, 09 Jan 2015 21:05:58 GMT
     }
-    const Checkpoints::CCheckpointData& Checkpoints() const 
+    const Checkpoints::CCheckpointData& Checkpoints() const
     {
         return dataTestnet;
     }
@@ -272,7 +281,7 @@ public:
         genesis.nNonce = 1096447;
         hashGenesisBlock = genesis.GetHash();
         nDefaultPort = 19994;
-        assert(hashGenesisBlock == uint256("0x5c13e22a0db8dc47ddb7b381ba85d7e5e17042a31af05f9c5f2fc932abcf71bd"));
+        assert(hashGenesisBlock == uint256("0x6cd6673143d8477b40839d1ff7d6dfc2a930b7d306463d65c66b091fbc803e99"));
 
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
@@ -285,7 +294,7 @@ public:
         fMineBlocksOnDemand = true;
         fTestnetToBeDeprecatedFieldRPC = false;
     }
-    const Checkpoints::CCheckpointData& Checkpoints() const 
+    const Checkpoints::CCheckpointData& Checkpoints() const
     {
         return dataRegtest;
     }
@@ -311,7 +320,7 @@ public:
         fMineBlocksOnDemand = true;
     }
 
-    const Checkpoints::CCheckpointData& Checkpoints() const 
+    const Checkpoints::CCheckpointData& Checkpoints() const
     {
         // UnitTest share the same checkpoints as MAIN
         return data;
@@ -373,3 +382,4 @@ bool SelectParamsFromCommandLine()
     SelectParams(network);
     return true;
 }
+
