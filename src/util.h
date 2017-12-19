8559e9dec1ece07d00546e806eae6d8f7d5b4f74
Dimannot <dimannot@bk.ru>
2017-12-19 22:15:36 +0300

    Update util.h

--- src/util.h
+++ src/util.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
 // Copyright (c) 2014-2017 The Dash Core developers
+// Copyright (c) 2017 The Not Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -12,7 +13,7 @@
 #define BITCOIN_UTIL_H
 
 #if defined(HAVE_CONFIG_H)
-#include "config/dash-config.h"
+#include "config/notcoin-config.h"
 #endif
 
 #include "compat.h"
@@ -248,7 +249,7 @@ std::string GetThreadName();
  */
 template <typename Callable> void TraceThread(const char* name,  Callable func)
 {
-    std::string s = strprintf("dash-%s", name);
+    std::string s = strprintf("notcoin-%s", name);
     RenameThread(s.c_str());
     try
     {
