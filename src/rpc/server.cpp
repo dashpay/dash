5c0ba6b20a54b42964c1249b7245a295759e730f
Dimannot <dimannot@bk.ru>
2017-12-20 01:20:28 +0300

    Update server.cpp

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -243,11 +243,11 @@ UniValue stop(const UniValue& params, bool fHelp)
     if (fHelp || params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "\nStop Dash Core server.");
+            "\nStop Not Core server.");
     // Event loop will exit after current HTTP requests have been handled, so
     // this reply will get back to the client.
     StartShutdown();
-    return "Dash Core server stopping";
+    return "Not Core server stopping";
 }
 
 /**
@@ -343,20 +343,20 @@ static const CRPCCommand vRPCCommands[] =
     { "hidden",             "resendwallettransactions", &resendwallettransactions, true},
 #endif
 
-    /* Dash features */
-    { "dash",               "masternode",             &masternode,             true  },
-    { "dash",               "masternodelist",         &masternodelist,         true  },
-    { "dash",               "masternodebroadcast",    &masternodebroadcast,    true  },
-    { "dash",               "gobject",                &gobject,                true  },
-    { "dash",               "getgovernanceinfo",      &getgovernanceinfo,      true  },
-    { "dash",               "getsuperblockbudget",    &getsuperblockbudget,    true  },
-    { "dash",               "voteraw",                &voteraw,                true  },
-    { "dash",               "mnsync",                 &mnsync,                 true  },
-    { "dash",               "spork",                  &spork,                  true  },
-    { "dash",               "getpoolinfo",            &getpoolinfo,            true  },
-    { "dash",               "sentinelping",           &sentinelping,           true  },
+    /* Notcoin features */
+    { "notcoin",               "masternode",             &masternode,             true  },
+    { "notcoin",               "masternodelist",         &masternodelist,         true  },
+    { "notcoin",               "masternodebroadcast",    &masternodebroadcast,    true  },
+    { "notcoin",               "gobject",                &gobject,                true  },
+    { "notcoin",               "getgovernanceinfo",      &getgovernanceinfo,      true  },
+    { "notcoin",               "getsuperblockbudget",    &getsuperblockbudget,    true  },
+    { "notcoin",               "voteraw",                &voteraw,                true  },
+    { "notcoin",               "mnsync",                 &mnsync,                 true  },
+    { "notcoin",               "spork",                  &spork,                  true  },
+    { "notcoin",               "getpoolinfo",            &getpoolinfo,            true  },
+    { "notcoin",               "sentinelping",           &sentinelping,           true  },
 #ifdef ENABLE_WALLET
-    { "dash",               "privatesend",            &privatesend,            false },
+    { "notcoin",               "privatesend",            &privatesend,            false },
 
     /* Wallet */
     { "wallet",             "keepass",                &keepass,                true },
@@ -580,7 +580,7 @@ std::vector<std::string> CRPCTable::listCommands() const
 
 std::string HelpExampleCli(const std::string& methodname, const std::string& args)
 {
-    return "> dash-cli " + methodname + " " + args + "\n";
+    return "> notcoin-cli " + methodname + " " + args + "\n";
 }
 
 std::string HelpExampleRpc(const std::string& methodname, const std::string& args)
