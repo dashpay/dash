4841b372ab95abc4749d0a6568868dbc62090d9f
UdjinM6 <UdjinM6@users.noreply.github.com>
2018-03-20 22:12:33 +0300

    address review comment

--- src/privatesend-server.cpp
+++ src/privatesend-server.cpp
@@ -292,6 +292,12 @@ void CPrivateSendServer::CheckPool(CConnman& connman)
         CommitFinalTransaction(connman);
         return;
     }
+
+    // reset if we're here for 10 seconds
+    if((nState == POOL_STATE_ERROR || nState == POOL_STATE_SUCCESS) && GetTimeMillis() - nTimeLastSuccessfulStep >= 10000) {
+        LogPrint("privatesend", "CPrivateSendServer::CheckPool -- timeout, RESETTING\n");
+        SetNull();
+    }
 }
 
 void CPrivateSendServer::CreateFinalTransaction(CConnman& connman)
