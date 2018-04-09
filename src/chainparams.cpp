9728710db1cbeff3514b9de63e60fbf0cacd1037
UdjinM6 <UdjinM6@users.noreply.github.com>
2018-04-09 13:18:08 +0300

    bump testnet minchainwork/assumevalid

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -328,10 +328,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_BIP147].nThreshold = 50; // 50% of 100
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000000924e924a21715"); // 37900
+        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000002951ef46f9553b"); // 100000
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x0000000004f5aef732d572ff514af99a995702c92e4452c7af10858231668b1f"); // 37900
+        consensus.defaultAssumeValid = uint256S("0x0000000003aa53e24b6e60ef97642e4193611f2bcb75ea1fa8105f0b5ffd5242"); // 100000
 
         pchMessageStart[0] = 0xce;
         pchMessageStart[1] = 0xe2;
