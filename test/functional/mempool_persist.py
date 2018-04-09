fe0c35d89dd78e5c78bf9c3941c1b68ab7357faa
MarcoFalke <falke.marco@gmail.com>
2018-04-09 14:37:01 -0400

    Merge #12917: qa: Windows fixups for functional tests
    fab9095d40 qa: Windows fixups for functional tests (MarcoFalke)
    
    Pull request description:
    
      Just two minor fixups to have less errors when the tests run on native windows.
      * Strip whitespace from lines when reading from a notification file
      * Instead of clumsily creating a file with weird permissions, just create a folder for the same effect in `mempool_persist.py`
    
    Tree-SHA512: 48a8b439f14ab9b44c5cd228cd03105e8613e703e3c2951cdf724931bc95172a9ad9bfe69fc23e73dd91b058c1352263c0ac6e8de2ceb0ebf804c8ff52bba394

--- test/functional/mempool_persist.py
+++ test/functional/mempool_persist.py
@@ -103,13 +103,14 @@ class MempoolPersistTest(BitcoinTestFramework):
         self.start_node(1, extra_args=[])
         wait_until(lambda: len(self.nodes[1].getrawmempool()) == 5)
 
-        self.log.debug("Prevent dashd from writing mempool.dat to disk. Verify that `savemempool` fails")
+        self.log.debug("Prevent bitcoind from writing mempool.dat to disk. Verify that `savemempool` fails")
         # to test the exception we are creating a tmp folder called mempool.dat.new
         # which is an implementation detail that could change and break this test
         mempooldotnew1 = mempooldat1 + '.new'
         os.mkdir(mempooldotnew1)
         assert_raises_rpc_error(-1, "Unable to dump mempool to disk", self.nodes[1].savemempool)
         os.rmdir(mempooldotnew1)
+
 
 if __name__ == '__main__':
     MempoolPersistTest().main()
