9c117935f334d6cc17c9c2d3c15dea1d84e78f5c
MarcoFalke <falke.marco@gmail.com>
2018-05-22 06:44:29 -0400

    Merge #13282: trivial: Mark overrides as such.
    60ebc7da4c trivial: Mark overrides as such. (Daniel Kraft)
    
    Pull request description:
    
      This trivial change adds the `override` keyword to some methods that override virtual base class / interface methods.  This ensures that any future changes to the interface's method signatures which are not correctly mirrored in the subclasses will break at compile time with a clear error message, rather than at runtime.
    
    Tree-SHA512: cc1bfa5f03b5e29d20e3eab07b0b5fa2f77b47f79e08263dbff43e4f463e9dd8f4f537e2c8c9b6cb3663220dcf40cfd77723cd9fcbd623c9efc90a4cd44facfc

--- src/bench/bench.h
+++ src/bench/bench.h
@@ -84,8 +84,29 @@ namespace benchmark {
     public:
         BenchRunner(std::string name, BenchFunction func);
 
-        static void RunAll(duration elapsedTimeForOne = std::chrono::seconds(1));
-    };
+// default printer to console, shows min, max, median.
+class ConsolePrinter : public Printer
+{
+public:
+    void header() override;
+    void result(const State& state) override;
+    void footer() override;
+};
+
+// creates box plot with plotly.js
+class PlotlyPrinter : public Printer
+{
+public:
+    PlotlyPrinter(std::string plotly_url, int64_t width, int64_t height);
+    void header() override;
+    void result(const State& state) override;
+    void footer() override;
+
+private:
+    std::string m_plotly_url;
+    int64_t m_width;
+    int64_t m_height;
+};
 }
 
 // BENCHMARK(foo) expands to:  benchmark::BenchRunner bench_11foo("foo", foo);
