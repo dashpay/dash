73673238de2a9c4076c18cee6086cf72fc0ad8be
InhumanPerfection <39734219+InhumanPerfection@users.noreply.github.com>
2018-06-20 21:31:46 +0300

    PrepareDenominate fix

--- src/privatesend-client.cpp
+++ src/privatesend-client.cpp
@@ -1166,8 +1166,8 @@ bool CPrivateSendClient::PrepareDenominate(int nMinRounds, int nMaxRounds, std::
                 ++it2;
             }
         }
-        if(nValueLeft == 0) break;
         nStep++;
+        if(nValueLeft == 0) break;
     }
 
     {
