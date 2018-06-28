2f93313f48567569b8bb8e745b5749f79ff5a0e9
MarcoFalke <falke.marco@gmail.com>
2018-06-29 10:08:52 +1400

    Merge #13563: bench: Simplify CoinSelection
    c2e4fc84ec bench: Simplify CoinSelection (João Barbosa)
    
    Pull request description:
    
      Closes #13549.
    
      As pointed by @MarcoFalke:
       - `SelectCoinsMinConf` should always succeed as there are enough coins in the wallet.
       - Removed creating the coins in the wallet.
    
    Tree-SHA512: 965c363bcaf0ca7a1dec35b5cf4866abcf190c53eb7012dc4aeb4d29830f13a7465644bfb5a47f6ea3eaa86e4d4a57fe41e7b2593bf5094b76a551c4c71625bb

--- src/bench/coin_selection.cpp
+++ src/bench/coin_selection.cpp
@@ -33,23 +33,22 @@ static void addCoin(const CAmount& nValue, const CWallet& wallet, std::vector<CO
 static void CoinSelection(benchmark::State& state)
 {
     const CWallet wallet("dummy", WalletDatabase::CreateDummy());
-    std::vector<COutput> vCoins;
     LOCK(wallet.cs_wallet);
 
+    // Add coins.
+    std::vector<COutput> vCoins;
+    for (int i = 0; i < 1000; ++i) {
+        addCoin(1000 * COIN, wallet, vCoins);
+    }
+    addCoin(3 * COIN, wallet, vCoins);
+
+    const CoinEligibilityFilter filter_standard(1, 6, 0);
+    const CoinSelectionParams coin_selection_params(true, 34, 148, CFeeRate(0), 0);
     while (state.KeepRunning()) {
-        // Empty wallet.
-        for (COutput output : vCoins)
-            delete output.tx;
-        vCoins.clear();
-
-        // Add coins.
-        for (int i = 0; i < 1000; i++)
-            addCoin(1000 * COIN, wallet, vCoins);
-        addCoin(3 * COIN, wallet, vCoins);
-
         std::set<CInputCoin> setCoinsRet;
         CAmount nValueRet;
-        bool success = wallet.SelectCoinsMinConf(1003 * COIN, 1, 6, 0, vCoins, setCoinsRet, nValueRet);
+        bool bnb_used;
+        bool success = wallet.SelectCoinsMinConf(1003 * COIN, filter_standard, vCoins, setCoinsRet, nValueRet, coin_selection_params, bnb_used);
         assert(success);
         assert(nValueRet == 1003 * COIN);
         assert(setCoinsRet.size() == 2);
