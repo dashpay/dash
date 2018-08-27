dashpay/dash-dev-branches

Allow new ProTxs to have a protocol version slightly higher then local version
Otherwise masternodes won't be able to upgrade as their ProTx would be
rejected by all nodes.

src/evo/providertx.cpp
@@ -26,7 +26,7 @@ bool CheckProviderTxRegister(const CTransaction& tx, const CBlockIndex* pindex, 
 
     if (ptx.nVersion != CProviderTXRegisterMN::CURRENT_VERSION)
         return state.DoS(100, false, REJECT_INVALID, "bad-provider-version");
-    if (ptx.nProtocolVersion < MIN_EVO_PROTO_VERSION || ptx.nProtocolVersion > PROTOCOL_VERSION)
+    if (ptx.nProtocolVersion < MIN_EVO_PROTO_VERSION || ptx.nProtocolVersion > MAX_PROTX_PROTO_VERSION)
         return state.DoS(10, false, REJECT_INVALID, "bad-provider-proto-version");
 
     if (ptx.nCollateralIndex < 0 || ptx.nCollateralIndex >= tx.vout.size())

dashpay/dash

src/evo/providertx.cpp
@@ -20,7 +20,7 @@
 template <typename ProTx>
 static bool CheckService(const ProTx& proTx, const CBlockIndex* pindex, CValidationState& state)
 {
-    if (proTx.nProtocolVersion < MIN_PROTX_PROTO_VERSION || proTx.nProtocolVersion > PROTOCOL_VERSION)
+    if (proTx.nProtocolVersion < MIN_PROTX_PROTO_VERSION || proTx.nProtocolVersion > MAX_PROTX_PROTO_VERSION)
         return state.DoS(10, false, REJECT_INVALID, "bad-protx-proto-version");
 
     if (!proTx.addr.IsValid())