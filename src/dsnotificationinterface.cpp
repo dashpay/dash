35fbede692753c2ffc28d2792f69f01e30b14188
Alexander Block <ablock84@gmail.com>
2018-10-25 19:25:10 +0200

    Fix wrong use of VersionBitsState auto IX

--- src/dsnotificationinterface.cpp
+++ src/dsnotificationinterface.cpp
@@ -45,7 +45,7 @@ void CDSNotificationInterface::UpdatedBlockTip(const CBlockIndex *pindexNew, con
     fDIP0001ActiveAtTip = pindexNew->nHeight >= Params().GetConsensus().DIP0001Height;
     // update instantsend autolock activation flag
     instantsend.isAutoLockBip9Active =
-            (VersionBitsState(pindexNew->pprev, Params().GetConsensus(), Consensus::DEPLOYMENT_ISAUTOLOCKS, versionbitscache) == THRESHOLD_ACTIVE);
+            (VersionBitsState(pindexNew, Params().GetConsensus(), Consensus::DEPLOYMENT_ISAUTOLOCKS, versionbitscache) == THRESHOLD_ACTIVE);
 
     if (fInitialDownload)
         return;
