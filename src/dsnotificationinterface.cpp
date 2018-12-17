d2447f9bfc27ea4f02778e4460f10fc774dd009a
Alexander Block <ablock84@gmail.com>
2018-12-17 19:22:48 +0100

    Split up remaining logic from CMasternodeMan into CMasternodeMetaMan and CMasternodeUtils
    Also get rid of CMastermode and store remaining meta info
    in CMasternodeMetaInfo

--- src/dsnotificationinterface.cpp
+++ src/dsnotificationinterface.cpp
@@ -6,13 +6,13 @@
 #include "dsnotificationinterface.h"
 #include "instantx.h"
 #include "governance.h"
-#include "masternodeman.h"
 #include "masternode-payments.h"
 #include "masternode-sync.h"
 #include "privatesend.h"
 #ifdef ENABLE_WALLET
 #include "privatesend-client.h"
 #endif // ENABLE_WALLET
+#include "validation.h"
 
 #include "evo/deterministicmns.h"
 
@@ -56,7 +56,6 @@ void CDSNotificationInterface::UpdatedBlockTip(const CBlockIndex *pindexNew, con
     if (fLiteMode)
         return;
 
-    mnodeman.UpdatedBlockTip(pindexNew);
     CPrivateSend::UpdatedBlockTip(pindexNew);
 #ifdef ENABLE_WALLET
     privateSendClient.UpdatedBlockTip(pindexNew);
