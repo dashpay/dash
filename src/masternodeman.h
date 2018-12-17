916d8921679686c4cd06aa80c708db08e39f2bbe
Alexander Block <ablock84@gmail.com>
2018-12-17 12:01:19 +0100

    Remove MNB/MNP/MNVERIFY related code from masternode(man).h/cpp

--- src/masternodeman.h
+++ src/masternodeman.h
@@ -33,13 +33,6 @@ private:
     static const int MAX_POSE_RANK              = 10;
     static const int MAX_POSE_BLOCKS            = 10;
 
-    static const int MNB_RECOVERY_QUORUM_TOTAL      = 10;
-    static const int MNB_RECOVERY_QUORUM_REQUIRED   = 6;
-    static const int MNB_RECOVERY_MAX_ASK_ENTRIES   = 10;
-    static const int MNB_RECOVERY_WAIT_SECONDS      = 60;
-    static const int MNB_RECOVERY_RETRY_SECONDS     = 3 * 60 * 60;
-
-
     // critical section to protect the inner data structures
     mutable CCriticalSection cs;
 
@@ -48,23 +41,6 @@ private:
 
     // map to hold all MNs
     std::map<COutPoint, CMasternode> mapMasternodes;
-    // who's asked for the Masternode list and the last time
-    std::map<CService, int64_t> mAskedUsForMasternodeList;
-    // who we asked for the Masternode list and the last time
-    std::map<CService, int64_t> mWeAskedForMasternodeList;
-    // which Masternodes we've asked for
-    std::map<COutPoint, std::map<CService, int64_t> > mWeAskedForMasternodeListEntry;
-
-    // who we asked for the masternode verification
-    std::map<CService, CMasternodeVerification> mWeAskedForVerification;
-
-    // these maps are used for masternode recovery from MASTERNODE_NEW_START_REQUIRED state
-    std::map<uint256, std::pair< int64_t, std::set<CService> > > mMnbRecoveryRequests;
-    std::map<uint256, std::vector<CMasternodeBroadcast> > mMnbRecoveryGoodReplies;
-    std::list< std::pair<CService, uint256> > listScheduledMnbRequestConnections;
-    std::map<CService, std::pair<int64_t, std::set<uint256> > > mapPendingMNB;
-    std::map<CService, std::pair<int64_t, CMasternodeVerification> > mapPendingMNV;
-    CCriticalSection cs_mapPendingMNV;
 
     /// Set when masternodes are added, cleared when CGovernanceManager is notified
     bool fMasternodesAdded;
@@ -74,26 +50,13 @@ private:
 
     std::vector<uint256> vecDirtyGovernanceObjectHashes;
 
-    int64_t nLastSentinelPingTime;
-
     friend class CMasternodeSync;
     /// Find an entry
     CMasternode* Find(const COutPoint& outpoint);
 
     bool GetMasternodeScores(const uint256& nBlockHash, score_pair_vec_t& vecMasternodeScoresRet, int nMinProtocol = 0);
 
-    void SyncSingle(CNode* pnode, const COutPoint& outpoint, CConnman& connman);
-    void SyncAll(CNode* pnode, CConnman& connman);
-
-    void PushDsegInvs(CNode* pnode, const CMasternode& mn);
-
 public:
-    // Keep track of all broadcasts I've seen
-    std::map<uint256, std::pair<int64_t, CMasternodeBroadcast> > mapSeenMasternodeBroadcast;
-    // Keep track of all pings I've seen
-    std::map<uint256, CMasternodePing> mapSeenMasternodePing;
-    // Keep track of all verifications I've seen
-    std::map<uint256, CMasternodeVerification> mapSeenMasternodeVerification;
     // keep track of dsq count to prevent masternodes from gaming privatesend queue
     int64_t nDsqCount;
 
@@ -113,16 +76,8 @@ public:
         }
 
         READWRITE(mapMasternodes);
-        READWRITE(mAskedUsForMasternodeList);
-        READWRITE(mWeAskedForMasternodeList);
-        READWRITE(mWeAskedForMasternodeListEntry);
-        READWRITE(mMnbRecoveryRequests);
-        READWRITE(mMnbRecoveryGoodReplies);
-        READWRITE(nLastSentinelPingTime);
         READWRITE(nDsqCount);
 
-        READWRITE(mapSeenMasternodeBroadcast);
-        READWRITE(mapSeenMasternodePing);
         if(ser_action.ForRead() && (strVersion != SERIALIZATION_VERSION_STRING)) {
             Clear();
         }
@@ -133,20 +88,8 @@ public:
     /// Add an entry
     bool Add(CMasternode &mn);
 
-    /// Ask (source) node for mnb
-    void AskForMN(CNode *pnode, const COutPoint& outpoint, CConnman& connman);
-
-    bool PoSeBan(const COutPoint &outpoint);
     bool AllowMixing(const COutPoint &outpoint);
     bool DisallowMixing(const COutPoint &outpoint);
-
-    /// Check all Masternodes
-    void Check();
-
-    /// Check all Masternodes and remove inactive
-    void CheckAndRemove(CConnman& connman);
-    /// This is dummy overload to be used for dumping/loading mncache.dat
-    void CheckAndRemove() {}
 
     void AddDeterministicMasternodes();
     void RemoveNonDeterministicMasternodes();
@@ -160,11 +103,6 @@ public:
     /// Count enabled Masternodes filtered by nProtocolVersion.
     /// Masternode nProtocolVersion should match or be above the one specified in param here.
     int CountEnabled(int nProtocolVersion = -1);
-
-    /// Count Masternodes by network type - NET_IPV4, NET_IPV6, NET_TOR
-    // int CountByIP(int nNetworkType);
-
-    void DsegUpdate(CNode* pnode, CConnman& connman);
 
     /// Versions of Find that are safe to use from outside the class
     bool Get(const COutPoint& outpoint, CMasternode& masternodeRet);
@@ -190,26 +128,11 @@ public:
     bool GetMasternodeRank(const COutPoint &outpoint, int& nRankRet, uint256& blockHashRet, int nBlockHeight = -1, int nMinProtocol = 0);
 
     void ProcessMasternodeConnections(CConnman& connman);
-    std::pair<CService, std::set<uint256> > PopScheduledMnbRequestConnection();
-    void ProcessPendingMnbRequests(CConnman& connman);
-
-    void DoFullVerificationStep(CConnman& connman);
-    void CheckSameAddr();
-    bool CheckVerifyRequestAddr(const CAddress& addr, CConnman& connman);
-    void PrepareVerifyRequest(const CAddress& addr, CConnman& connman);
-    void ProcessPendingMnvRequests(CConnman& connman);
-    void SendVerifyReply(CNode* pnode, CMasternodeVerification& mnv, CConnman& connman);
-    void ProcessVerifyReply(CNode* pnode, CMasternodeVerification& mnv);
-    void ProcessVerifyBroadcast(CNode* pnode, const CMasternodeVerification& mnv);
 
     /// Return the number of (unique) Masternodes
     int size() { return mapMasternodes.size(); }
 
     std::string ToString() const;
-
-    /// Perform complete check and only then update masternode list and maps using provided CMasternodeBroadcast
-    bool CheckMnbAndUpdateMasternodeList(CNode* pfrom, CMasternodeBroadcast mnb, int& nDos, CConnman& connman);
-    bool IsMnbRecoveryRequested(const uint256& hash) { return mMnbRecoveryRequests.count(hash); }
 
     void UpdateLastPaid(const CBlockIndex* pindex);
 
@@ -227,19 +150,10 @@ public:
         return vecTmp;
     }
 
-    bool IsSentinelPingActive();
-    void UpdateLastSentinelPingTime();
     bool AddGovernanceVote(const COutPoint& outpoint, uint256 nGovernanceObjectHash);
     void RemoveGovernanceObject(uint256 nGovernanceObjectHash);
 
-    void CheckMasternode(const CKeyID& keyIDOperator, bool fForce);
-
-    bool IsMasternodePingedWithin(const COutPoint& outpoint, int nSeconds, int64_t nTimeToCheckAt = -1);
-    void SetMasternodeLastPing(const COutPoint& outpoint, const CMasternodePing& mnp);
-
     void UpdatedBlockTip(const CBlockIndex *pindex);
-
-    void WarnMasternodeDaemonUpdates();
 
     /**
      * Called to notify CGovernanceManager that the masternode index has been updated.
