2b4a149d9a00b348537878eb981c9afee1ed5ffc
MeshCollider <dobsonsa68@gmail.com>
2018-12-18 15:33:41 +1300

    Merge #14957: wallet: Initialize stop_block in CWallet::ScanForWalletTransactions
    8b9171ccf wallet: Initialize stop_block to nullptr in CWallet::ScanForWalletTransactions (Ben Woosley)
    
    Pull request description:
    
      Previously the argument would be untouched if the first block scan failed. This
      makes the behavior predictable, and consistent with the documentation.
    
    Tree-SHA512: 3efadf9fd5e25ecd9450f32545f58e61a123ad883e921ef427b13e4782ffdd8ffe905c9ad3edc7e8f9e4953342cd72247bb4cc9eeaf9e5fd04291ac5c1bb5eec
    
    # Conflicts:
    #	src/wallet/test/wallet_tests.cpp

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2157,6 +2157,7 @@ CWallet::ScanResult CWallet::ScanForWalletTransactions(const CBlockIndex* const 
 
     const CBlockIndex* pindex = pindexStart;
     failed_block = nullptr;
+    stop_block = nullptr;
 
     if (pindex) WalletLogPrintf("Rescan started from block %d...\n", pindex->nHeight);
 
