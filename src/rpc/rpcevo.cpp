5b5eb3ae19586202597c2f79916087822f7d88cf
UdjinM6 <UdjinM6@users.noreply.github.com>
2019-01-28 13:38:28 +0300

    static const

--- src/rpc/rpcevo.cpp
+++ src/rpc/rpcevo.cpp
@@ -32,7 +32,7 @@ extern UniValue sendrawtransaction(const JSONRPCRequest& request);
 
 std::string GetHelpString(int nParamNum, std::string strParamName)
 {
-    std::map<std::string, std::string> mapParamHelp = {
+    static const std::map<std::string, std::string> mapParamHelp = {
         {"collateralAddress",
             "%d. \"collateralAddress\"        (string, required) The dash address to send the collateral to.\n"
             "                              Must be a P2PKH address.\n"
