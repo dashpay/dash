bd0b1524c2a1f5e4d329e27eb281d96fc32d523b
UdjinM6 <UdjinM6@users.noreply.github.com>
2019-03-07 14:22:20 +0300

    Add `AssertLockHeld(cs_main); // for mapBlockIndex`

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -66,6 +66,7 @@ void WalletTxToJSON(const CWalletTx& wtx, UniValue& entry)
     bool fLocked = instantsend.IsLockedInstantSendTransaction(wtx.GetHash());
     bool chainlock = false;
     if (confirms > 0) {
+        AssertLockHeld(cs_main); // for mapBlockIndex
         chainlock = llmq::chainLocksHandler->HasChainLock(mapBlockIndex[wtx.hashBlock]->nHeight, wtx.hashBlock);
     }
     entry.push_back(Pair("confirmations", confirms));
