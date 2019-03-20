2a8d2845547371d2940b93d488814057a9d6dc4c
Alexander Block <ablock84@gmail.com>
2019-03-20 11:23:12 +0100

    Introduce "qsendrecsigs" to indicate that plain recovered sigs should be sent
    Full nodes, including masternodes, will send this message automatically.
    Other node implementations (e.g. SPV) are usually not interested and would
    not send this message.

--- src/net.h
+++ src/net.h
@@ -817,6 +817,8 @@ public:
     // Whether a ping is requested.
     std::atomic<bool> fPingQueued;
 
+    // If true, we will announce/send him plain recovered sigs (usually true for full nodes)
+    std::atomic<bool> fSendRecSigs{false};
     // If true, we will send him all quorum related messages, even if he is not a member of our quorums
     std::atomic_bool qwatch{false};
 
