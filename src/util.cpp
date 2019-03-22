e3b007556f9285ec8dc3ca250a0253bc5940e998
Alexander Block <ablock84@gmail.com>
2019-03-22 16:01:53 +0100

    Use llmq-sig log category for CSigSharesManager

--- src/util.cpp
+++ src/util.cpp
@@ -286,6 +286,7 @@ bool LogAcceptCategory(const char* category)
                     ptrCategory->insert(std::string("instantsend"));
                     ptrCategory->insert(std::string("keepass"));
                     ptrCategory->insert(std::string("llmq"));
+                    ptrCategory->insert(std::string("llmq-sigs"));
                     ptrCategory->insert(std::string("masternode"));
                     ptrCategory->insert(std::string("mnpayments"));
                     ptrCategory->insert(std::string("mnsync"));
