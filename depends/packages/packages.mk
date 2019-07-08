6d6b585affb9c3dca2865daa881785a646331653
Wladimir J. van der Laan <laanwj@gmail.com>
2019-07-08 13:19:39 +0200

    Merge #16352: build: prune dbus from depends
    e8fabd9253400a7c3fe45b34bc572eb00ff5522d build: prune dbus from depends (fanquake)
    
    Pull request description:
    
      Since #8210 (59d063d07660d6e8d53ed8aa1eb8b0747ea6767c), we've been passing `-dbus-runtime` when configuring Qt.
    
      ```
      qtbase-opensource-src-5.9.7 $ ./configure -h | grep -i dbus
        -no-dbus ............. Do not build the Qt D-Bus module
        -dbus-linked ......... Build Qt D-Bus and link to libdbus-1 [auto]
        -dbus-runtime ........ Build Qt D-Bus and dynamically load libdbus-1 [no]
      ```
    
      This means we don't actually seem to be using the `D-Bus` we build in depends. This was pointed out by theuni at the time, [here](https://github.com/bitcoin/bitcoin/pull/7993#issuecomment-223114395) and [here](https://github.com/bitcoin/bitcoin/pull/8210#issuecomment-226930545), but was never followed up. dongcarl also bought it up as part of #16150.
    
      I've tested building and running `bitcoin-qt` using depends on Debian. Needs further testing.
    
    ACKs for top commit:
      laanwj:
        code review ACK e8fabd9253400a7c3fe45b34bc572eb00ff5522d
    
    Tree-SHA512: 164e6e52b6f97c04aef42bd185e2a157bc1a42103840f9404c5a795749f45a8c2c35f35873395a3a56398b3cd5955496b90d9c885d929b434c9bc871695abe20
    
    # Conflicts:
    #	depends/packages/dbus.mk
    #	depends/packages/packages.mk
    #	doc/dependencies.md

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,7 +3,7 @@ packages:=boost openssl libevent zeromq gmp bls-dash backtrace cmake immer
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig
+qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
