33eb2bb41ed690ea7df36aa6fe523a26018502ad
Enrique Berrueta <eabz@polispay.org>
2020-05-02 08:54:26 -0500

    Revert "WIP qt upgrade"
    This reverts commit 7d27e60baf8b9626f76d2d493f23185b41f98bb9.

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_QT_WALLETMODEL_H
 #define BITCOIN_QT_WALLETMODEL_H
 
+#include "paymentrequestplus.h"
 #include "walletmodeltransaction.h"
 
 #ifdef ENABLE_WALLET
@@ -58,6 +59,8 @@ public:
     // If from a payment request, this is used for storing the memo
     QString message;
 
+    // If from a payment request, paymentRequest.IsInitialized() will be true
+    PaymentRequestPlus paymentRequest;
     // Empty if no authentication or invalid signature/cert/etc.
     QString authenticatedMerchant;
 
@@ -73,6 +76,9 @@ public:
         std::string sAddress = address.toStdString();
         std::string sLabel = label.toStdString();
         std::string sMessage = message.toStdString();
+        std::string sPaymentRequest;
+        if (!ser_action.ForRead() && paymentRequest.IsInitialized())
+            paymentRequest.SerializeToString(&sPaymentRequest);
         std::string sAuthenticatedMerchant = authenticatedMerchant.toStdString();
 
         READWRITE(this->nVersion);
