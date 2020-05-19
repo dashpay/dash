2c412b69597e745f5726a3e099374919e711d00e
Pasta <pasta@dashboost.org>
2020-05-19 14:26:01 -0500

    implement increased MAX_HEADERS_RESULTS
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/validation.h
+++ src/validation.h
@@ -88,8 +88,10 @@ static const int MAX_BLOCKS_IN_TRANSIT_PER_PEER = 16;
 /** Timeout in seconds during which a peer must stall block download progress before being disconnected. */
 static const unsigned int BLOCK_STALLING_TIMEOUT = 2;
 /** Number of headers sent in one getheaders result. We rely on the assumption that if a peer sends
- *  less than this number, we reached its tip. Changing this value is a protocol upgrade. */
-static const unsigned int MAX_HEADERS_RESULTS = 2000;
+ *  less than this number, we reached its tip. Changing this value is a protocol upgrade.
+ *  TODO remove MAX_HEADERS_RESULTS_OLD once minimum protocol version is 70218 */
+static const unsigned int MAX_HEADERS_RESULTS_OLD = 2000;
+static const unsigned int MAX_HEADERS_RESULTS = 8000;
 /** Maximum depth of blocks we're willing to serve as compact blocks to peers
  *  when requested. For older blocks, a regular BLOCK response will be sent. */
 static const int MAX_CMPCTBLOCK_DEPTH = 5;
