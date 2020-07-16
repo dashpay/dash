0d2e6b104fe70d716ce60a7cd40d83f1f87f3ecd
pasta <pasta@dashboost.org>
2020-07-16 11:17:57 -0500

    rename feature_privatesend.py to rpc_privatesend.py
    Signed-off-by: pasta <pasta@dashboost.org>

--- test/functional/rpc_privatesend.py
+++ test/functional/rpc_privatesend.py
@@ -7,7 +7,7 @@ from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal
 
 '''
-feature_privatesend.py
+rpc_privatesend.py
 
 Tests PrivateSend basic RPC
 '''
--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -149,7 +149,7 @@ BASE_SCRIPTS= [
     'feature_minchainwork.py',
     'p2p_unrequested_blocks.py', # NOTE: needs dash_hash to pass
     'feature_shutdown.py',
-    'feature_privatesend.py',
+    'rpc_privatesend.py',
     'p2p_fingerprint.py',
     'feature_uacomment.py',
     'p2p_unrequested_blocks.py',
