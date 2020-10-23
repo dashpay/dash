b21f400746f276c9f7799b32895d2a512e4db14e
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-10-23 03:24:58 +0300

    Move helper functions out of run_test

--- test/functional/feature_llmq_simplepose.py
+++ test/functional/feature_llmq_simplepose.py
@@ -29,10 +29,7 @@ class LLMQSimplePoSeTest(DashTestFramework):
         self.test_no_banning(expected_connections=2)
 
         # Now lets isolate MNs one by one and verify that punishment/banning happens
-        def isolate_mn(mn):
-            mn.node.setnetworkactive(False)
-            wait_until(lambda: mn.node.getconnectioncount() == 0)
-        self.test_banning(isolate_mn, 1, True)
+        self.test_banning(self.isolate_mn, 1, True)
 
         self.repair_masternodes(False)
 
@@ -45,26 +42,32 @@ class LLMQSimplePoSeTest(DashTestFramework):
         self.test_no_banning(expected_connections=4)
 
         # Lets restart masternodes with closed ports and verify that they get banned even though they are connected to other MNs (via outbound connections)
-        def close_mn_port(mn):
-            self.stop_node(mn.node.index)
-            self.start_masternode(mn, ["-listen=0", "-nobind"])
-            connect_nodes(mn.node, 0)
-            # Make sure the to-be-banned node is still connected well via outbound connections
-            for mn2 in self.mninfo:
-                if mn2 is not mn:
-                    connect_nodes(mn.node, mn2.node.index)
-            self.reset_probe_timeouts()
-        self.test_banning(close_mn_port, 3, False)
+        self.test_banning(self.close_mn_port, 3, False)
 
         self.repair_masternodes(True)
         self.reset_probe_timeouts()
 
-        def force_old_mn_proto(mn):
-            self.stop_node(mn.node.index)
-            self.start_masternode(mn, ["-pushversion=70216"])
-            connect_nodes(mn.node, 0)
-            self.reset_probe_timeouts()
-        self.test_banning(force_old_mn_proto, 3, False)
+        self.test_banning(self.force_old_mn_proto, 3, False)
+
+    def isolate_mn(self, mn):
+        mn.node.setnetworkactive(False)
+        wait_until(lambda: mn.node.getconnectioncount() == 0)
+
+    def close_mn_port(self, mn):
+        self.stop_node(mn.node.index)
+        self.start_masternode(mn, ["-listen=0", "-nobind"])
+        connect_nodes(mn.node, 0)
+        # Make sure the to-be-banned node is still connected well via outbound connections
+        for mn2 in self.mninfo:
+            if mn2 is not mn:
+                connect_nodes(mn.node, mn2.node.index)
+        self.reset_probe_timeouts()
+
+    def force_old_mn_proto(self, mn):
+        self.stop_node(mn.node.index)
+        self.start_masternode(mn, ["-pushversion=70216"])
+        connect_nodes(mn.node, 0)
+        self.reset_probe_timeouts()
 
     def test_no_banning(self, expected_connections):
         for i in range(3):
