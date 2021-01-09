97f348f3b0af5dc136e7be22237512f06f823098
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2021-01-08 22:37:37 -0700

    Update src/rpc/mining.cpp
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -197,7 +197,7 @@ UniValue generatetoaddress(const JSONRPCRequest& request)
 #else
 UniValue generatetoaddress(const JSONRPCRequest& request)
 {
-    throw std::runtime_error("Error RPC miner isn't compiled");
+    throw JSONRPCError(RPC_METHOD_NOT_FOUND, "This call is not available because RPC miner isn't compiled");
 }
 #endif // ENABLE_MINER
 
