c656fef75076ab5e3a4d535c76d97a5265a3c6ee
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-01-19 01:02:45 +0300

    Prettify node extra args

--- test/functional/feature_multikeysporks.py
+++ test/functional/feature_multikeysporks.py
@@ -48,42 +48,22 @@ class MultiKeySporkTest(BitcoinTestFramework):
 
         self.add_nodes(5)
 
-        self.node0_extra_args = ["-sporkkey=931wyuRNVYvhg18Uu9bky5Qg1z4QbxaJ7fefNBzjBPiLRqcd33F",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"]
+        spork_chain_params =   ["-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
+                                "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
+                                "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
+                                "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
+                                "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
+                                "-minsporkkeys=3"]
+
+        # Node0 extra args to use on normal node restarts
+        self.node0_extra_args = ["-sporkkey=931wyuRNVYvhg18Uu9bky5Qg1z4QbxaJ7fefNBzjBPiLRqcd33F"] + spork_chain_params
+
         self.start_node(0, self.node0_extra_args)
-        self.start_node(1, ["-sporkkey=91vbXGMSWKGHom62986XtL1q2mQDA12ngcuUNNe5NfMSj44j7g3",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
-        self.start_node(2, ["-sporkkey=92bxUjPT5AhgXuXJwfGGXqhomY2SdQ55MYjXyx9DZNxCABCSsRH",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
-        self.start_node(3, ["-sporkkey=934yPXiVGf4RCY2qTs2Bt5k3TEtAiAg12sMxCt8yVWbSU7p3fuD",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
-        self.start_node(4, ["-sporkkey=92Cxwia363Wg2qGF1fE5z4GKi8u7r1nrWQXdtsj2ACZqaDPSihD",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
+        self.start_node(1, ["-sporkkey=91vbXGMSWKGHom62986XtL1q2mQDA12ngcuUNNe5NfMSj44j7g3"] + spork_chain_params)
+        self.start_node(2, ["-sporkkey=92bxUjPT5AhgXuXJwfGGXqhomY2SdQ55MYjXyx9DZNxCABCSsRH"] + spork_chain_params)
+        self.start_node(3, ["-sporkkey=934yPXiVGf4RCY2qTs2Bt5k3TEtAiAg12sMxCt8yVWbSU7p3fuD"] + spork_chain_params)
+        self.start_node(4, ["-sporkkey=92Cxwia363Wg2qGF1fE5z4GKi8u7r1nrWQXdtsj2ACZqaDPSihD"] + spork_chain_params)
+
         # connect nodes at start
         for i in range(0, 5):
             for j in range(i, 5):
