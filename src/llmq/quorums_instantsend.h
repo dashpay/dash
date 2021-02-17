ebbcf05299894a3eacf830afa88e46f6bf63f869
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-02-17 11:35:27 +0300

    Update src/llmq/quorums_instantsend.h
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>

--- src/llmq/quorums_instantsend.h
+++ src/llmq/quorums_instantsend.h
@@ -60,7 +60,6 @@ public:
     static void WriteInstantSendLockArchived(CDBBatch& batch, const uint256& hash, int nHeight);
     std::unordered_map<uint256, CInstantSendLockPtr> RemoveConfirmedInstantSendLocks(int nUntilHeight);
     void RemoveArchivedInstantSendLocks(int nUntilHeight);
-
     bool KnownInstantSendLock(const uint256& islockHash) const;
     size_t GetInstantSendLockCount() const;
 
