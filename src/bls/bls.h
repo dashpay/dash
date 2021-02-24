742599339217b277226ac1c339f59424c55e1399
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-02-24 03:52:05 +0300

    bls: Drop UpdateHash methods
    Make sure the hash is updated via GetHash() only.

--- src/bls/bls.h
+++ src/bls/bls.h
@@ -121,14 +121,9 @@ public:
     const uint256& GetHash() const
     {
         if (cachedHash.IsNull()) {
-            UpdateHash();
+            cachedHash = ::SerializeHash(*this);
         }
         return cachedHash;
-    }
-
-    void UpdateHash() const
-    {
-        cachedHash = ::SerializeHash(*this);
     }
 
     bool SetHexStr(const std::string& str)
@@ -418,16 +413,11 @@ public:
             hash.SetNull();
         }
         if (hash.IsNull()) {
-            UpdateHash();
+            CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
+            ss.write((const char*)vecBytes.data(), vecBytes.size());
+            hash = ss.GetHash();
         }
         return hash;
-    }
-private:
-    void UpdateHash() const
-    {
-        CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
-        ss.write((const char*)vecBytes.data(), vecBytes.size());
-        hash = ss.GetHash();
     }
 };
 typedef CBLSLazyWrapper<CBLSSignature> CBLSLazySignature;
