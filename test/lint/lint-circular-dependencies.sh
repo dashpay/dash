UdjinM6/dash_unforked

Make linter happy

test/lint/lint-circular-dependencies.sh
@@ -77,6 +77,7 @@ EXPECTED_CIRCULAR_DEPENDENCIES=(
     "llmq/quorums -> llmq/quorums_utils -> llmq/quorums"
     "llmq/quorums_blockprocessor -> net_processing -> llmq/quorums_blockprocessor"
     "llmq/quorums_chainlocks -> llmq/quorums_instantsend -> llmq/quorums_chainlocks"
+    "llmq/quorums_chainlocks -> masternode/activemasternode -> validationinterface -> llmq/quorums_chainlocks"
     "llmq/quorums_chainlocks -> net_processing -> llmq/quorums_chainlocks"
     "llmq/quorums_chainlocks -> validation -> llmq/quorums_chainlocks"
     "llmq/quorums_dkgsessionmgr -> net_processing -> llmq/quorums_dkgsessionmgr"
@@ -111,7 +112,6 @@ EXPECTED_CIRCULAR_DEPENDENCIES=(
     "httprpc -> httpserver -> init -> httprpc"
     "httpserver -> init -> httpserver"
     "init -> llmq/quorums_init -> llmq/quorums_signing_shares -> init"
-    "llmq/quorums_chainlocks -> net_processing -> validationinterface -> llmq/quorums_chainlocks"
     "llmq/quorums_chainlocks -> llmq/quorums_instantsend -> wallet/wallet -> llmq/quorums_chainlocks"
     "llmq/quorums_dkgsession -> llmq/quorums_dkgsessionmgr -> llmq/quorums_dkgsessionhandler -> llmq/quorums_dkgsession"
     "llmq/quorums_instantsend -> net_processing -> validationinterface -> llmq/quorums_instantsend"

dashpay/dash

test/lint/lint-circular-dependencies.sh
@@ -116,7 +116,7 @@ EXPECTED_CIRCULAR_DEPENDENCIES=(
     "coinjoin/coinjoin -> masternode/activemasternode -> net -> coinjoin/coinjoin"
     "evo/deterministicmns -> validationinterface -> txmempool -> evo/deterministicmns"
     "llmq/quorums -> validation -> llmq/quorums_chainlocks -> llmq/quorums"
-    "llmq/quorums_chainlocks -> llmq/quorums_instantsend -> validation -> llmq/quorums_chainlocks"
+    "llmq/quorums_chainlocks -> llmq/quorums_commitment -> validation -> llmq/quorums_chainlocks"
     "evo/deterministicmns -> evo/simplifiedmns -> llmq/quorums_blockprocessor -> net_processing -> masternode/masternode-payments -> evo/deterministicmns"
     "core_io -> evo/cbtx -> llmq/quorums_blockprocessor -> net_processing -> masternode/masternode-payments -> governance/governance-classes -> core_io"
     "evo/deterministicmns -> evo/simplifiedmns -> llmq/quorums_blockprocessor -> net_processing -> masternode/masternode-payments -> masternode/activemasternode -> evo/deterministicmns"