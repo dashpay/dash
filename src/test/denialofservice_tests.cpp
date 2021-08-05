0abe2c9228b89c5111ff9e5157fe79c8167dbfb7
fanquake <fanquake@gmail.com>
2021-08-05 15:58:51 +0800

    Merge bitcoin/bitcoin#22630: test: Add missing include
    6666ec9e059ea66065a11f5e25c0a17d7d0834fe test: Add missing include (MarcoFalke)
    
    Pull request description:
    
      Fix the silent merge conflict introduced in commit 7d60f7ec6bbf293c6f23f442a0fd985111a15ff7
    
    ACKs for top commit:
      fanquake:
        ACK 6666ec9e059ea66065a11f5e25c0a17d7d0834fe - fixes:
    
    Tree-SHA512: c712715db4a41cf6ed991fb35482d8fd9016b5a27e1309654018219ebbfd8d4f98a389bdf5d7b792e7a4690928aeef943c716465b0c5e00f70320ad2e134ebb5

--- src/test/denialofservice_tests.cpp
+++ src/test/denialofservice_tests.cpp
@@ -235,7 +235,7 @@ BOOST_AUTO_TEST_CASE(block_relay_only_eviction)
 {
     const CChainParams& chainparams = Params();
     auto connman = std::make_unique<ConnmanTestMsg>(0x1337, 0x1337, *m_node.addrman);
-    auto peerLogic = PeerManager::make(chainparams, *connman, *m_node.addrman, nullptr, *m_node.scheduler,
+    auto peerLogic = PeerManager::make(chainparams, *connman, *m_node.addrman, nullptr,
                                        *m_node.chainman, *m_node.mempool, *m_node.mn_metaman, *m_node.mn_sync,
                                        *m_node.govman, *m_node.sporkman, /* mn_activeman = */ nullptr, m_node.dmnman,
                                        m_node.cj_ctx, m_node.llmq_ctx, /* ignore_incoming_txs = */ false);
