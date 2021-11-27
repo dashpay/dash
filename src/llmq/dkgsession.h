17924d788f53c4b1d1865fa6566677bdcf7cc229
pasta <pasta@dashboost.org>
2021-11-26 22:51:09 -0500

    use const ref
    Signed-off-by: pasta <pasta@dashboost.org>

--- src/llmq/dkgsession.h
+++ src/llmq/dkgsession.h
@@ -190,7 +190,7 @@ public:
 class CDKGMember
 {
 public:
-    CDKGMember(CDeterministicMNCPtr _dmn, size_t _idx);
+    CDKGMember(const CDeterministicMNCPtr& _dmn, size_t _idx);
 
     CDeterministicMNCPtr dmn;
     size_t idx;
