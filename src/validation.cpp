7f2c84b7becbc610b23e573f6d96bc1d5b26ae8c
William Casarin <jb55@jb55.com>
2021-12-19 12:52:59 -0800

    block_connected: don't serialize block hash twice
    In the validation:block_connected tracepoint, we call block->GetHash(),
    which ends up calling CBlockHeader::GetHash(), executing around 8000
    serialization instructions. We don't need to do this extra work, because
    block->GetHash() is already called further up in the function. Let's
    save that value as a local variable and re-use it in our tracepoint so
    there is no unnecessary tracepoint overhead.
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- src/validation.cpp
+++ src/validation.cpp
@@ -1978,7 +1978,10 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
 
     AssertLockHeld(cs_main);
     assert(pindex);
-    assert(*pindex->phashBlock == block.GetHash());
+
+    uint256 block_hash{block.GetHash()};
+    assert(*pindex->phashBlock == block_hash);
+
     assert(m_clhandler);
     assert(m_isman);
     assert(m_quorum_block_processor);
@@ -2026,7 +2029,7 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
 
     // Special case for the genesis block, skipping connection of its transactions
     // (its coinbase is unspendable)
-    if (block.GetHash() == chainparams.GetConsensus().hashGenesisBlock) {
+    if (block_hash == chainparams.GetConsensus().hashGenesisBlock) {
         if (!fJustCheck)
             view.SetBestBlock(pindex->GetBlockHash());
         return true;
