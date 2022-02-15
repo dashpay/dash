777c919dcbd3c5740ddde14ab9c209f9617b71d2
PastaPastaPasta <PastaPastaPasta@users.noreply.github.com>
2022-02-15 23:41:32 +0530

    evo: introduce IsTriviallyValid() to CheckPro*Tx objects

--- src/evo/providertx.cpp
+++ src/evo/providertx.cpp
@@ -392,6 +392,42 @@ bool CheckProUpRevTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CVal
     return true;
 }
 
+maybe_error CProRegTx::IsTriviallyValid() const
+{
+    if (nVersion == 0 || nVersion > CProRegTx::CURRENT_VERSION) {
+        return {100, "bad-protx-version"};
+    }
+    if (nType != 0) {
+        return {100, "bad-protx-type"};
+    }
+    if (nMode != 0) {
+        return {100, "bad-protx-mode"};
+    }
+
+    if (keyIDOwner.IsNull() || !pubKeyOperator.IsValid() || keyIDVoting.IsNull()) {
+        return {10, "bad-protx-key-null"};
+    }
+    if (!scriptPayout.IsPayToPublicKeyHash() && !scriptPayout.IsPayToScriptHash()) {
+        return {10, "bad-protx-payee"};
+    }
+
+    CTxDestination payoutDest;
+    if (!ExtractDestination(scriptPayout, payoutDest)) {
+        // should not happen as we checked script types before
+        return {10, "bad-protx-payee-dest"};
+    }
+    // don't allow reuse of payout key for other keys (don't allow people to put the payee key onto an online server)
+    if (payoutDest == CTxDestination(keyIDOwner) || payoutDest == CTxDestination(keyIDVoting)) {
+        return {10, "bad-protx-payee-reuse"};
+    }
+
+    if (nOperatorReward > 10000) {
+        return {10, "bad-protx-operator-reward"};
+    }
+
+    return {};
+}
+
 std::string CProRegTx::MakeSignString() const
 {
     std::string s;
@@ -429,6 +465,15 @@ std::string CProRegTx::ToString() const
         nVersion, collateralOutpoint.ToStringShort(), addr.ToString(), (double)nOperatorReward / 100, EncodeDestination(keyIDOwner), pubKeyOperator.ToString(), EncodeDestination(keyIDVoting), payee);
 }
 
+maybe_error CProUpServTx::IsTriviallyValid() const
+{
+    if (nVersion == 0 || nVersion > CProUpServTx::CURRENT_VERSION) {
+        return {100, "bad-protx-version"};
+    }
+
+    return {};
+}
+
 std::string CProUpServTx::ToString() const
 {
     CTxDestination dest;
@@ -441,6 +486,24 @@ std::string CProUpServTx::ToString() const
         nVersion, proTxHash.ToString(), addr.ToString(), payee);
 }
 
+maybe_error CProUpRegTx::IsTriviallyValid() const
+{
+    if (nVersion == 0 || nVersion > CProUpRegTx::CURRENT_VERSION) {
+        return {100, "bad-protx-version"};
+    }
+    if (nMode != 0) {
+        return {100, "bad-protx-mode"};
+    }
+
+    if (!pubKeyOperator.IsValid() || keyIDVoting.IsNull()) {
+        return {10, "bad-protx-key-null"};
+    }
+    if (!scriptPayout.IsPayToPublicKeyHash() && !scriptPayout.IsPayToScriptHash()) {
+        return {10, "bad-protx-payee"};
+    }
+    return {};
+}
+
 std::string CProUpRegTx::ToString() const
 {
     CTxDestination dest;
@@ -451,6 +514,20 @@ std::string CProUpRegTx::ToString() const
 
     return strprintf("CProUpRegTx(nVersion=%d, proTxHash=%s, pubKeyOperator=%s, votingAddress=%s, payoutAddress=%s)",
         nVersion, proTxHash.ToString(), pubKeyOperator.ToString(), EncodeDestination(keyIDVoting), payee);
+}
+
+maybe_error CProUpRevTx::IsTriviallyValid() const
+{
+    if (nVersion == 0 || nVersion > CProUpRevTx::CURRENT_VERSION) {
+        return {100, "bad-protx-version"};
+    }
+
+    // nReason < CProUpRevTx::REASON_NOT_SPECIFIED is always `false` since
+    // nReason is unsigned and CProUpRevTx::REASON_NOT_SPECIFIED == 0
+    if (nReason > CProUpRevTx::REASON_LAST) {
+        return {100, "bad-protx-reason"};
+    }
+    return {};
 }
 
 std::string CProUpRevTx::ToString() const
