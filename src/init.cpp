490b02b5f48b6486e027a0bfdc0f0b4c29ea4096
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-03-25 03:37:03 +0530

    merge bitcoin#17407: Add reference to mempool in NodeContext

--- src/init.cpp
+++ src/init.cpp
@@ -386,6 +386,7 @@ void Shutdown(NodeContext& node)
     // Shutdown part 2: delete wallet instance
     globalVerifyHandle.reset();
     ECC_Stop();
+    if (node.mempool) node.mempool = nullptr;
     LogPrintf("%s: done\n", __func__);
 }
 
@@ -2175,6 +2176,11 @@ bool AppInitMain(NodeContext& node)
         LogPrintf("Shutdown requested. Exiting.\n");
         return false;
     }
+
+    // Now that the chain state is loaded, make mempool generally available in the node context. For example the
+    // connection manager, wallet, or RPC threads, which are all started after this, may use it from the node context.
+    assert(!node.mempool);
+    node.mempool = &::mempool;
 
     fs::path est_path = GetDataDir() / FEE_ESTIMATES_FILENAME;
     CAutoFile est_filein(fsbridge::fopen(est_path, "rb"), SER_DISK, CLIENT_VERSION);
