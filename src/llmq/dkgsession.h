2956da41cce1ddaf0d1166d0879235a2908b4bf4
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2022-04-05 19:25:08 +0530

    Handling of quorumIndex

--- src/llmq/dkgsession.h
+++ src/llmq/dkgsession.h
@@ -146,6 +146,7 @@ public:
     Consensus::LLMQType llmqType{Consensus::LLMQType::LLMQ_NONE};
     uint256 quorumHash;
     uint256 proTxHash;
+    uint32_t quorumIndex; // used to identify quorums of the same type. Example: if 64 quorums of the same type are active at the same time, quorumIndex has a value [0,63]
     std::vector<bool> validMembers;
 
     CBLSPublicKey quorumPublicKey;
@@ -171,6 +172,7 @@ public:
                 obj.llmqType,
                 obj.quorumHash,
                 obj.proTxHash,
+                obj.quorumIndex,
                 DYNBITSET(obj.validMembers),
                 obj.quorumPublicKey,
                 obj.quorumVvecHash,
@@ -236,6 +238,7 @@ private:
     CDKGSessionManager& dkgManager;
 
     const CBlockIndex* m_quorum_base_block_index{nullptr};
+    uint32_t quorumIndex{};
 
 private:
     std::vector<std::unique_ptr<CDKGMember>> members;
@@ -275,7 +278,7 @@ public:
     CDKGSession(const Consensus::LLMQParams& _params, CBLSWorker& _blsWorker, CDKGSessionManager& _dkgManager) :
         params(_params), blsWorker(_blsWorker), cache(_blsWorker), dkgManager(_dkgManager) {}
 
-    bool Init(const CBlockIndex* pQuorumBaseBlockIndex, const std::vector<CDeterministicMNCPtr>& mns, const uint256& _myProTxHash);
+    bool Init(const CBlockIndex* pQuorumBaseBlockIndex, const std::vector<CDeterministicMNCPtr>& mns, const uint256& _myProTxHash, const uint32_t _quorumIndex = 0);
 
     std::optional<size_t> GetMyMemberIndex() const { return myIdx; }
 
