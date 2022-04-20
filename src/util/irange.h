f9508b42188f317520f262ca9c11a5834e32d04b
pasta <pasta@dashboost.org>
2022-04-20 13:40:49 -0500

    misc fixes

--- src/util/irange.h
+++ src/util/irange.h
@@ -1,8 +1,7 @@
 // Copyright (c) 2021 The Dash Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-// Original implemtation here: https://github.com/whoshuu/cpp_range under MIT software license
-
+// Original implementation here: https://github.com/whoshuu/cpp_range/blob/cd6897e40176c3031ad1e4c8069d672b2e544996/include/range.h under MIT software license
 
 #ifndef BITCOIN_UTIL_IRANGE_H
 #define BITCOIN_UTIL_IRANGE_H
