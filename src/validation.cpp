a554090468d5b56faa7cad941ebd69a5dbe8eb0c
Konstantin Akimov <github@knstqq.com>
2022-05-20 19:39:33 +0100

    partial Merge bitcoin/bitcoin#25064: [kernel 2b/n] Add `ChainstateManager::m_adjusted_time_callback`
    BACKPORT NOTE:
    next dependencies are still there:
     - netaddress.cpp
     - netbase.cpp
     - timedata.cpp
    
    There's tiny script to find-out who is exactly blocker:
    
    find . -name '*kernel*.o' -exec bash -c "nm -C -u '{}' 2>/dev/null | grep -qF 'GetSporkValue(' && echo '{}'" \;
    
    GetAdjustedTime is used by governance/vote, governance/object, spork_manager
    CNetAddress, CService are used by evo/netinfo
    ----
    
    53494bc7392591336e09d095f1fc38d63d566abf validation: Have ChainstateManager own m_chainparams (Carl Dong)
    04c31c1295eb4ecd42afd54b8e353cbda93d83f0 Add ChainstateManager::m_adjusted_time_callback (Carl Dong)
    dbe45c34f8b4fd7d615f7e05ef1454798ef0c8ca Add ChainstateManagerOpts, using as ::Options (Carl Dong)
    
    Pull request description:
    
      ```
      This decouples validation.cpp from netaddress.cpp (transitively,
      timedata.cpp, and asmap.cpp).
    
      This is important for libbitcoinkernel as:
    
      - There is no reason for the consensus engine to be coupled with
        netaddress, timedata, and asmap
      - Users of libbitcoinkernel can now easily supply their own
        std::function that provides the adjusted time.
    
      See the src/Makefile.am changes for some satisfying removals.
      ```
    
    Top commit has no ACKs.
    
    Tree-SHA512: a093ec6ecacdc659d656574f05bd31ade6a6cdb64d5a97684f94ae7e55c0e360b78177553d4d1ef40280192674464d029a0d68e96caf8711d9274011172f1330
    
    Co-authored-by: MacroFake <falke.marco@gmail.com>

--- src/validation.cpp
+++ src/validation.cpp
@@ -33,8 +33,6 @@
 #include <script/script.h>
 #include <script/sigcache.h>
 #include <shutdown.h>
-
-#include <timedata.h>
 #include <tinyformat.h>
 #include <txdb.h>
 #include <txmempool.h>
@@ -2208,7 +2206,7 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
     // Also, currently the rule against blocks more than 2 hours in the future
     // is enforced in ContextualCheckBlockHeader(); we wouldn't want to
     // re-enforce that rule here (at least until we make it impossible for
-    // GetAdjustedTime() to go backward).
+    // m_adjusted_time_callback() to go backward).
     if (!CheckBlock(block, state, m_params.GetConsensus(), !fJustCheck, !fJustCheck)) {
         if (state.GetResult() == BlockValidationResult::BLOCK_MUTATED) {
             // We don't write down blocks to disk if they may have been
@@ -4069,7 +4067,7 @@ bool ChainstateManager::AcceptBlockHeader(const CBlockHeader& block, BlockValida
             return state.Invalid(BlockValidationResult::BLOCK_CHAINLOCK, "bad-prevblk-chainlock");
         }
 
-        if (!ContextualCheckBlockHeader(block, state, m_blockman, *this, pindexPrev, GetAdjustedTime())) {
+        if (!ContextualCheckBlockHeader(block, state, m_blockman, *this, pindexPrev, m_adjusted_time_callback())) {
             LogPrint(BCLog::VALIDATION, "%s: Consensus::ContextualCheckBlockHeader: %s, %s\n", __func__, hash.ToString(), state.ToString());
             return false;
         }
@@ -4335,6 +4333,7 @@ bool TestBlockValidity(BlockValidationState& state,
                        CChainState& chainstate,
                        const CBlock& block,
                        CBlockIndex* pindexPrev,
+                       const std::function<int64_t()>& adjusted_time_callback,
                        bool fCheckPOW,
                        bool fCheckMerkleRoot)
 {
@@ -4363,7 +4362,7 @@ bool TestBlockValidity(BlockValidationState& state,
     auto dbTx = evoDb.BeginTransaction();
 
     // NOTE: CheckBlockHeader is called by CheckBlock
-    if (!ContextualCheckBlockHeader(block, state, chainstate.m_blockman, chainstate.m_chainman, pindexPrev, GetAdjustedTime()))
+    if (!ContextualCheckBlockHeader(block, state, chainstate.m_blockman, chainstate.m_chainman, pindexPrev, adjusted_time_callback()))
         return error("%s: Consensus::ContextualCheckBlockHeader: %s", __func__, state.ToString());
     if (!CheckBlock(block, state, chainparams.GetConsensus(), fCheckPOW, fCheckMerkleRoot))
         return error("%s: Consensus::CheckBlock: %s", __func__, state.ToString());
