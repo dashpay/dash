ac1bf66f1c504551848aa0e50e56f29b0a3d9064
UdjinM6 <UdjinM6@users.noreply.github.com>
2022-06-23 22:35:13 +0300

    throw an error when a llmq type with an incompatible rotation flag is picked for `-llmq...` params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1230,6 +1230,9 @@ void CDevNetParams::UpdateDevnetLLMQChainLocksFromArgs(const ArgsManager& args)
     Consensus::LLMQType llmqType = Consensus::LLMQType::LLMQ_NONE;
     for (const auto& params : consensus.llmqs) {
         if (params.name == strLLMQType) {
+            if (params.useRotation) {
+                throw std::runtime_error("LLMQ type specified for -llmqchainlocks must NOT use rotation");
+            }
             llmqType = params.type;
         }
     }
@@ -1248,7 +1251,10 @@ void CDevNetParams::UpdateDevnetLLMQInstantSendFromArgs(const ArgsManager& args)
 
     Consensus::LLMQType llmqType = Consensus::LLMQType::LLMQ_NONE;
     for (const auto& params : consensus.llmqs) {
-        if (params.name == strLLMQType && !params.useRotation) {
+        if (params.name == strLLMQType) {
+            if (params.useRotation) {
+                throw std::runtime_error("LLMQ type specified for -llmqinstantsend must NOT use rotation");
+            }
             llmqType = params.type;
         }
     }
@@ -1267,7 +1273,10 @@ void CDevNetParams::UpdateDevnetLLMQInstantSendDIP0024FromArgs(const ArgsManager
 
     Consensus::LLMQType llmqType = Consensus::LLMQType::LLMQ_NONE;
     for (const auto& params : consensus.llmqs) {
-        if (params.name == strLLMQType && params.useRotation) {
+        if (params.name == strLLMQType) {
+            if (!params.useRotation) {
+                throw std::runtime_error("LLMQ type specified for -llmqinstantsenddip0024 must use rotation");
+            }
             llmqType = params.type;
         }
     }
