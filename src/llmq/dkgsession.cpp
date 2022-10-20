3d2ec1b2e5e1734e87aa3c86ec596d305825a5d1
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2022-10-20 14:42:11 -0500

    style: fix colon location in for loop

--- src/llmq/dkgsession.cpp
+++ src/llmq/dkgsession.cpp
@@ -89,7 +89,7 @@ bool CDKGSession::Init(const CBlockIndex* _pQuorumBaseBlockIndex, const std::vec
         int cycleQuorumBaseHeight = m_quorum_base_block_index->nHeight - quorumIndex;
         const CBlockIndex* pCycleQuorumBaseBlockIndex = m_quorum_base_block_index->GetAncestor(cycleQuorumBaseHeight);
         std::stringstream ss;
-        for (const auto& mn: members) {
+        for (const auto& mn : members) {
             ss << mn->dmn->proTxHash.ToString().substr(0, 4) << " | ";
         }
         logger.Batch("DKGComposition h[%d] i[%d] DKG:%s", pCycleQuorumBaseBlockIndex->nHeight, quorumIndex, ss.str());
@@ -105,7 +105,7 @@ bool CDKGSession::Init(const CBlockIndex* _pQuorumBaseBlockIndex, const std::vec
         relayMembers = utils::GetQuorumRelayMembers(params, m_quorum_base_block_index, myProTxHash, true);
         if (LogAcceptCategory(BCLog::LLMQ)) {
             std::stringstream ss;
-            for (const auto& r: relayMembers) {
+            for (const auto& r : relayMembers) {
                 ss << r.ToString().substr(0, 4) << " | ";
             }
             logger.Batch("forMember[%s] relayMembers[%s]", myProTxHash.ToString().substr(0, 4), ss.str());
