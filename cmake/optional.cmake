bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

cmake: Add syscall sandbox support

cmake/optional.cmake
@@ -22,8 +22,10 @@ set(WITH_USDT "auto" CACHE STRING "Enable tracepoints for Userspace, Statically 
 
 set(WITH_QRENCODE "auto" CACHE STRING "Enable QR code support ([auto], yes, no). \"auto\" means \"yes\" if libqrencode is found")
 
+set(WITH_SECCOMP "auto" CACHE STRING "Enable experimental syscall sandbox feature (-sandbox) ([auto], yes, no). \"auto\" means \"yes\" if seccomp-bpf is found under Linux x86_64")
+
 set(OPTION_VALUES auto yes no)
-foreach(option USE_CCACHE WITH_SQLITE WITH_BDB WITH_NATPMP WITH_MINIUPNPC WITH_ZMQ WITH_USDT WITH_QRENCODE)
+foreach(option USE_CCACHE WITH_SQLITE WITH_BDB WITH_NATPMP WITH_MINIUPNPC WITH_ZMQ WITH_USDT WITH_QRENCODE WITH_SECCOMP)
   if(NOT ${option} IN_LIST OPTION_VALUES)
     message(FATAL_ERROR "${option} value is \"${${option}}\", but must be one of \"auto\", \"yes\" or \"no\".")
   endif()
@@ -165,5 +167,24 @@ if(NOT WITH_GUI STREQUAL "no" AND NOT WITH_QRENCODE STREQUAL "no")
       message(FATAL_ERROR "libqrencode requested, but not found.")
     endif()
     set(WITH_QRENCODE no)
+  endif()
+endif()
+
+if(NOT WITH_SECCOMP STREQUAL "no")
+  check_cxx_source_compiles("
+  #include <linux/seccomp.h>
+  #if !defined(__x86_64__)
+  #  error Syscall sandbox is an experimental feature currently available only under Linux x86-64.
+  #endif
+  int main(){}
+  " HAVE_SECCOMP_H)
+  if(HAVE_SECCOMP_H)
+    set(USE_SYSCALL_SANDBOX TRUE)
+    set(WITH_SECCOMP yes)
+  else()
+    if(WITH_SECCOMP STREQUAL "yes")
+      message(FATAL_ERROR "linux/seccomp.h requested, but not found.")
+    endif()
+    set(WITH_SECCOMP no)
   endif()
 endif()

dashpay/dash

cmake/optional.cmake
@@ -162,3 +162,22 @@ if(WITH_GUI AND WITH_QRENCODE)
     set(WITH_QRENCODE OFF)
   endif()
 endif()
+
+if(WITH_SECCOMP)
+  check_cxx_source_compiles("
+  #include <linux/seccomp.h>
+  #if !defined(__x86_64__)
+  #  error Syscall sandbox is an experimental feature currently available only under Linux x86-64.
+  #endif
+  int main(){}
+  " HAVE_SECCOMP_H)
+  if(HAVE_SECCOMP_H)
+    set(USE_SYSCALL_SANDBOX TRUE)
+    set(WITH_SECCOMP ON)
+  else()
+    if(WITH_SECCOMP STREQUAL ON)
+      message(FATAL_ERROR "linux/seccomp.h requested, but not found.")
+    endif()
+    set(WITH_SECCOMP OFF)
+  endif()
+endif()