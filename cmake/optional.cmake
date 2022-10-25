bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

cmake: Add `systemtap-sdt` optional package support

cmake/optional.cmake
@@ -18,8 +18,10 @@ option(ENABLE_UPNP_DEFAULT "If UPNP is enabled, turn it on at startup" OFF)
 
 set(WITH_ZMQ "auto" CACHE STRING "Enable ZMQ notifications ([auto], yes, no). \"auto\" means \"yes\" if libzmq is found")
 
+set(WITH_USDT "auto" CACHE STRING "Enable tracepoints for Userspace, Statically Defined Tracing ([auto], yes, no). \"auto\" means \"yes\" if sys/sdt.h is found")
+
 set(OPTION_VALUES auto yes no)
-foreach(option USE_CCACHE WITH_SQLITE WITH_BDB WITH_NATPMP WITH_MINIUPNPC WITH_ZMQ)
+foreach(option USE_CCACHE WITH_SQLITE WITH_BDB WITH_NATPMP WITH_MINIUPNPC WITH_ZMQ WITH_USDT)
   if(NOT ${option} IN_LIST OPTION_VALUES)
     message(FATAL_ERROR "${option} value is \"${${option}}\", but must be one of \"auto\", \"yes\" or \"no\".")
   endif()
@@ -125,5 +127,26 @@ if(NOT WITH_ZMQ STREQUAL "no")
                       "To skip libzmq check, use \"-DWITH_ZMQ=no\".\n")
     endif()
     set(WITH_ZMQ no)
+  endif()
+endif()
+
+include(CheckCXXSourceCompiles)
+if(NOT WITH_USDT STREQUAL "no")
+  check_cxx_source_compiles("
+  #include <sys/sdt.h>
+
+  int main()
+  {
+    DTRACE_PROBE(\"context\", \"event\");
+  }
+  " HAVE_USDT_H)
+  if(HAVE_USDT_H)
+    set(ENABLE_TRACING TRUE)
+    set(WITH_USDT yes)
+  else()
+    if(WITH_USDT STREQUAL "yes")
+      message(FATAL_ERROR "sys/sdt.h requested, but not found.")
+    endif()
+    set(WITH_USDT no)
   endif()
 endif()

dashpay/dash

cmake/optional.cmake
@@ -126,3 +126,24 @@ if(WITH_ZMQ)
     set(WITH_ZMQ OFF)
   endif()
 endif()
+
+include(CheckCXXSourceCompiles)
+if(WITH_USDT)
+  check_cxx_source_compiles("
+  #include <sys/sdt.h>
+
+  int main()
+  {
+    DTRACE_PROBE(\"context\", \"event\");
+  }
+  " HAVE_USDT_H)
+  if(HAVE_USDT_H)
+    set(ENABLE_TRACING TRUE)
+    set(WITH_USDT ON)
+  else()
+    if(WITH_USDT STREQUAL ON)
+      message(FATAL_ERROR "sys/sdt.h requested, but not found.")
+    endif()
+    set(WITH_USDT OFF)
+  endif()
+endif()