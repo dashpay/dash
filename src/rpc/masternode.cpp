cda144c3f16980b4a15a8efb9cb3ee5e66e9aa7f
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2022-12-29 11:08:35 +0200

    hide old banned mns by default

--- src/rpc/masternode.cpp
+++ src/rpc/masternode.cpp
@@ -35,6 +35,7 @@ static void masternode_list_help(const JSONRPCRequest& request)
         "Get a list of masternodes in different modes. This call is identical to 'masternode list' call.\n"
         "Available modes:\n"
         "  addr           - Print ip address associated with a masternode (can be additionally filtered, partial match)\n"
+        "  all            - Print info in JSON format for all masternodes (can be additionally filtered, partial match)\n"
         "  full           - Print info in format 'status payee lastpaidtime lastpaidblock IP'\n"
         "                   (can be additionally filtered, partial match)\n"
         "  info           - Print info in format 'status payee IP'\n"
@@ -560,7 +561,7 @@ static UniValue masternodelist(const JSONRPCRequest& request)
                 strMode != "owneraddress" && strMode != "votingaddress" &&
                 strMode != "lastpaidtime" && strMode != "lastpaidblock" &&
                 strMode != "payee" && strMode != "pubkeyoperator" &&
-                strMode != "status"))
+                strMode != "status" && strMode != "all"))
     {
         masternode_list_help(request);
     }
@@ -587,7 +588,16 @@ static UniValue masternodelist(const JSONRPCRequest& request)
         return (int)pindex->nTime;
     };
 
+    bool showAllBannedMns = strMode == "all";
+    int tipHeight = WITH_LOCK(cs_main, return ::ChainActive().Tip()->nHeight;);
+    constexpr int monthBlockLength = 24 * 24 * 30;
     mnList.ForEachMN(false, [&](auto& dmn) {
+        if (!showAllBannedMns && mnList.IsMNPoSeBanned(dmn)) {
+            if (tipHeight - dmn.pdmnState->GetBannedHeight() > monthBlockLength) {
+                return;
+            }
+        }
+
         std::string strOutpoint = dmn.collateralOutpoint.ToStringShort();
         Coin coin;
         std::string collateralAddressStr = "UNKNOWN";
@@ -634,7 +644,7 @@ static UniValue masternodelist(const JSONRPCRequest& request)
             if (strFilter !="" && strInfo.find(strFilter) == std::string::npos &&
                 strOutpoint.find(strFilter) == std::string::npos) return;
             obj.pushKV(strOutpoint, strInfo);
-        } else if (strMode == "json") {
+        } else if (strMode == "json" || strMode == "all") {
             std::ostringstream streamInfo;
             streamInfo <<  dmn.proTxHash.ToString() << " " <<
                            dmn.pdmnState->addr.ToString() << " " <<
