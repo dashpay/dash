ecfde2d0257a40be412d71c20ab625d32712f5af
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2023-02-16 12:03:56 +0200

    fix comment

--- src/llmq/params.h
+++ src/llmq/params.h
@@ -32,7 +32,7 @@ enum class LLMQType : uint8_t {
     // for testing only
     LLMQ_TEST_DIP0024 = 103,     // 4 members, 2 (66%) threshold, one per hour. Params might differ when -llmqtestparams is used
     LLMQ_TEST_INSTANTSEND = 104, // 3 members, 2 (66%) threshold, one per hour. Params might differ when -llmqtestinstantsendparams is used
-    LLMQ_TEST_PLATFORM = 106,    // 4 members, 2 (66%) threshold, one per hour.
+    LLMQ_TEST_PLATFORM = 106,    // 3 members, 2 (66%) threshold, one per hour.
 
     // for devnets only. rotated version (v2) for devnets
     LLMQ_DEVNET_DIP0024 = 105 // 8 members, 4 (50%) threshold, one per hour. Params might differ when -llmqdevnetparams is used
