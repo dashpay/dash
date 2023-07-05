2fa373eedee8a94dac5cc2e5264f8099ca674a00
pasta <pasta@dashboost.org>
2023-07-05 09:40:22 -0500

    remove unused param

--- src/evo/deterministicmns.cpp
+++ src/evo/deterministicmns.cpp
@@ -645,7 +645,7 @@ bool CDeterministicMNManager::ProcessBlock(const CBlock& block, const CBlockInde
     return true;
 }
 
-bool CDeterministicMNManager::UndoBlock(const CBlock& block, const CBlockIndex* pindex)
+bool CDeterministicMNManager::UndoBlock(const CBlockIndex* pindex)
 {
     int nHeight = pindex->nHeight;
     uint256 blockHash = pindex->GetBlockHash();
