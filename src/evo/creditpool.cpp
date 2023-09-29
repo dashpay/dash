65f5d461ea491deb4e89e1eb11a7ed9896ddc6c7
pasta <pasta@dashboost.org>
2023-09-28 20:36:18 -0500

    refactor: additional if-init usage

--- src/evo/creditpool.cpp
+++ src/evo/creditpool.cpp
@@ -249,14 +249,12 @@ bool CCreditPoolDiff::SetTarget(const CTransaction& tx, const CAmount blockRewar
 
 bool CCreditPoolDiff::Lock(const CTransaction& tx, TxValidationState& state)
 {
-    CAssetLockPayload assetLockTx;
-    if (!GetTxPayload(tx, assetLockTx)) {
+    if (CAssetLockPayload assetLockTx; !GetTxPayload(tx, assetLockTx)) {
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "failed-creditpool-lock-payload");
     }
 
     for (const CTxOut& txout : tx.vout) {
-        const CScript& script = txout.scriptPubKey;
-        if (script.empty() || script[0] != OP_RETURN) continue;
+        if (const CScript& script = txout.scriptPubKey; script.empty() || script[0] != OP_RETURN) continue;
 
         sessionLocked += txout.nValue;
         return true;
