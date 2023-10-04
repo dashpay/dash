8b1aef8307ef8908a2391fb2c276c20f07accfd9
pasta <pasta@dashboost.org>
2023-10-04 12:03:51 -0500

    fix constexpr_if_cxx20.h guards and copyright

--- src/constexpr_if_cxx20.h
+++ src/constexpr_if_cxx20.h
@@ -1,5 +1,9 @@
-#ifndef DASH_CONSTEXPR_IF_CXX20_H
-#define DASH_CONSTEXPR_IF_CXX20_H
+// Copyright (c) 2023 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CONSTEXPR_IF_CXX20_H
+#define BITCOIN_CONSTEXPR_IF_CXX20_H
 
 #if __cplusplus >= 202002L
 #define CONSTEXPR_IF_CPP20 constexpr
@@ -7,4 +11,4 @@
 #define CONSTEXPR_IF_CPP20
 #endif
 
-#endif //DASH_CONSTEXPR_IF_CXX20_H
+#endif //BITCOIN_CONSTEXPR_IF_CXX20_H
