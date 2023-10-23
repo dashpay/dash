5383e97aeeda5560d16438661c31035922b65f55
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2023-10-23 13:58:07 +0200

    fix

--- src/governance/classes.cpp
+++ src/governance/classes.cpp
@@ -495,15 +495,14 @@ CAmount CSuperblock::GetPaymentsLimit(int nBlockHeight)
     }
 
     const CBlockIndex* tipIndex = ::ChainActive().Tip();
-    bool fMNRewardReallocated = llmq::utils::IsMNRewardReallocationActive(tipIndex);
-    bool fV20Active = llmq::utils::IsV20Active(tipIndex);
-    if (!fMNRewardReallocated && nBlockHeight > tipIndex->nHeight) {
-        // If fMNRewardReallocated isn't active yet and nBlockHeight refers to a future SuperBlock
+    const auto v20_state = llmq::utils::GetV20State(tipIndex);
+    bool fV20Active{v20_state == ThresholdState::ACTIVE};
+    if (!fV20Active && nBlockHeight > tipIndex->nHeight) {
+        // If fV20Active isn't active yet and nBlockHeight refers to a future SuperBlock
         // then we need to check if the fork is locked_in and see if it will be active by the time of the future SuperBlock
-        if (llmq::utils::GetMNRewardReallocationState(tipIndex) == ThresholdState::LOCKED_IN) {
-            int activation_height = llmq::utils::GetMNRewardReallocationSince(tipIndex) + static_cast<int>(Params().GetConsensus().vDeployments[Consensus::DEPLOYMENT_MN_RR].nWindowSize);
+        if (v20_state == ThresholdState::LOCKED_IN) {
+            int activation_height = llmq::utils::GetV20Since(tipIndex) + static_cast<int>(Params().GetConsensus().vDeployments[Consensus::DEPLOYMENT_V20].nWindowSize);
             if (nBlockHeight >= activation_height) {
-                fMNRewardReallocated = true;
                 fV20Active = true;
             }
         }
