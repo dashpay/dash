02a9ff69ee93508c9246e006d851113f08c67341
pasta <pasta@dashboost.org>
2023-11-26 14:51:54 -0600

    fix: resolve linter complaints

--- src/evo/cbtx.cpp
+++ src/evo/cbtx.cpp
@@ -33,7 +33,7 @@ bool CheckCbTx(const CTransaction& tx, const CBlockIndex* pindexPrev, TxValidati
     if (!opt_cbTx) {
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-cbtx-payload");
     }
-    auto& cbTx = *opt_cbTx;
+    const auto& cbTx = *opt_cbTx;
 
     if (cbTx.nVersion == CCbTx::Version::INVALID || cbTx.nVersion >= CCbTx::Version::UNKNOWN) {
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-cbtx-version");
