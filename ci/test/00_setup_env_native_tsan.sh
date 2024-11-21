9d920c12843460b32f3db23214ae4508b367c21b
pasta <pasta@dashboost.org>
2024-11-20 23:45:20 -0600

    ci: use 2 jobs in fuzz test runner?

--- ci/test/00_setup_env_native_tsan.sh
+++ ci/test/00_setup_env_native_tsan.sh
@@ -10,7 +10,7 @@ export CONTAINER_NAME=ci_native_tsan
 export PACKAGES="clang-16 llvm-16 libclang-rt-16-dev libc++abi-16-dev libc++-16-dev python3-zmq"
 export DEP_OPTS="CC=clang-16 CXX='clang++-16 -stdlib=libc++'"
 export TEST_RUNNER_EXTRA="--extended --exclude feature_pruning,feature_dbcrash,wallet_multiwallet.py" # Temporarily suppress ASan heap-use-after-free (see issue #14163)
-export TEST_RUNNER_EXTRA="${TEST_RUNNER_EXTRA} --timeout-factor=4"  # Increase timeout because sanitizers slow down
+export TEST_RUNNER_EXTRA="${TEST_RUNNER_EXTRA} --timeout-factor=4 --jobs=2"  # Increase timeout because sanitizers slow down
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-zmq --with-gui=no --with-sanitizers=thread CC=clang-16 CXX=clang++-16 CXXFLAGS='-g' --with-boost-process"
 export CPPFLAGS="-DDEBUG_LOCKORDER -DARENA_DEBUG"
