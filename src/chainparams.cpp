ffc662ecd3266e1005e1e0a97cc3abf706044c31
Konstantin Akimov <knstqq@gmail.com>
2024-12-12 13:40:57 +0700

    feat: make possible to change CL quorum too

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -922,8 +922,10 @@ public:
 
         UpdateLLMQTestParametersFromArgs(args, Consensus::LLMQType::LLMQ_TEST);
         UpdateLLMQTestParametersFromArgs(args, Consensus::LLMQType::LLMQ_TEST_INSTANTSEND);
-        UpdateLLMQInstantSendDIP0024FromArgs(args);
-        UpdateLLMQTestPlatformFromArgs(args);
+
+        UpdateLLMQTypeFromArgs(args, "-llmqtestchainlocks", consensus.llmqTypeChainLocks);
+        UpdateLLMQTypeFromArgs(args, "-llmqtestinstantsenddip0024", consensus.llmqTypeDIP0024InstantSend);
+        UpdateLLMQTypeFromArgs(args, "-llmqtestplatform", consensus.llmqTypePlatform);
     }
 
     /**
@@ -987,8 +989,7 @@ public:
     }
 
     void UpdateLLMQTestParametersFromArgs(const ArgsManager& args, const Consensus::LLMQType llmqType);
-    void UpdateLLMQInstantSendDIP0024FromArgs(const ArgsManager& args);
-    void UpdateLLMQTestPlatformFromArgs(const ArgsManager& args);
+    void UpdateLLMQTypeFromArgs(const ArgsManager& args, const std::string& arg_name, Consensus::LLMQType& llmqTypeToSet);
 };
 
 static void MaybeUpdateHeights(const ArgsManager& args, Consensus::Params& consensus)
@@ -1164,15 +1165,11 @@ void CRegTestParams::UpdateLLMQTestParametersFromArgs(const ArgsManager& args, c
     UpdateLLMQTestParameters(size, threshold, llmqType);
 }
 
-void CRegTestParams::UpdateLLMQInstantSendDIP0024FromArgs(const ArgsManager& args)
+void CRegTestParams::UpdateLLMQTypeFromArgs(const ArgsManager& args, const std::string& arg_name, Consensus::LLMQType& llmqTypeToSet)
 {
-    if (!args.IsArgSet("-llmqtestinstantsenddip0024")) return;
+    if (!args.IsArgSet(arg_name)) return;
 
-    const auto& llmq_params_opt = GetLLMQ(consensus.llmqTypeDIP0024InstantSend);
-    assert(llmq_params_opt.has_value());
-
-    std::string strLLMQType = gArgs.GetArg("-llmqtestinstantsenddip0024", std::string(llmq_params_opt->name));
-
+    const std::string strLLMQType = gArgs.GetArg(arg_name, "");
     Consensus::LLMQType llmqType = Consensus::LLMQType::LLMQ_NONE;
     for (const auto& params : consensus.llmqs) {
         if (params.name == strLLMQType) {
@@ -1180,33 +1177,11 @@ void CRegTestParams::UpdateLLMQInstantSendDIP0024FromArgs(const ArgsManager& arg
         }
     }
     if (llmqType == Consensus::LLMQType::LLMQ_NONE) {
-        throw std::runtime_error("Invalid LLMQ type specified for -llmqtestinstantsenddip0024.");
+        throw std::runtime_error(strprintf("Invalid LLMQ type specified for %s", arg_name));
     }
-    LogPrintf("Setting llmqtestinstantsenddip0024 to %ld\n", ToUnderlying(llmqType));
 
-    consensus.llmqTypeDIP0024InstantSend = llmqType;
-}
-
-void CRegTestParams::UpdateLLMQTestPlatformFromArgs(const ArgsManager& args)
-{
-    if (!args.IsArgSet("-llmqtestplatform")) return;
-
-    const auto& llmq_params_opt = GetLLMQ(consensus.llmqTypePlatform);
-    assert(llmq_params_opt.has_value());
-
-    std::string strLLMQType = gArgs.GetArg("-llmqtestplatform", std::string(llmq_params_opt->name));
-
-    Consensus::LLMQType llmqType = Consensus::LLMQType::LLMQ_NONE;
-    for (const auto& params : consensus.llmqs) {
-        if (params.name == strLLMQType) {
-            llmqType = params.type;
-        }
-    }
-    if (llmqType == Consensus::LLMQType::LLMQ_NONE) {
-        throw std::runtime_error("Invalid LLMQ type specified for -llmqtestplatform.");
-    }
-    LogPrintf("Setting llmqtestplatform to size=%ld\n", static_cast<uint8_t>(llmqType));
-    consensus.llmqTypePlatform = llmqType;
+    LogPrintf("Setting %s to %ld\n", arg_name, ToUnderlying(llmqType));
+    llmqTypeToSet = llmqType;
 }
 
 void CDevNetParams::UpdateDevnetSubsidyAndDiffParametersFromArgs(const ArgsManager& args)
