6854947e93e675ef5542530bb57e79d64a788f0d
pasta <pasta@dashboost.org>
2025-05-06 08:57:32 -0500

    Merge #6653: perf: optimize usage of deterministic mn manager for UndoBlock operation
    df01820fd1bfc23a9e7dcb07ccaa86e8de4bdb03 perf: optimize deterministic mn manager for UndoBlock operation (Konstantin Akimov)
    
    Pull request description:
    
      ## Issue being fixed or feature implemented
      Depends on height of block, `undo` requires calculation of diff for Deterministic MN List in the range [1...575 * 2].
      Moreover, most of these calculations are done twice for each block (chain for block H and H-1 from the last known snapshot).
    
      ## What was done?
      `curList` is calculated based on known diff and `prevList` instead of calculation from known snapshot.
    
      ## How Has This Been Tested?
      Run on develop for 3000 blocks to undo. This PR speeds up undo-time from 28 seconds to just 18 (35% faster).
    
       - develop
      ```
      2025-04-29T10:31:41Z UpdateTip: new best=000000000000000977893146a932cd866063231e9e0601b6ef2cf33318b6a34f height=2262927 version=0x20000000 log2_work=79.388301 tx=57646017 date='2025-04-29T10:25:42Z' progress=0.999999 cache=0.5MiB(3194txo) evodb_cache=0.0MiB
      ...
      2025-04-29T10:32:09Z UpdateTip: new best=000000000000003599418914c28f8516a8a44a9fb690861d2aaec47ce5cd7e23 height=2259999 version=0x20000000 log2_work=79.386510 tx=57583502 date='2025-04-24T02:34:58Z' progress=0.998779 cache=5.7MiB(48249txo) evodb_cache=0.0MiB
      ```
    
       - PR
      ```
      2025-04-29T10:43:04Z UpdateTip: new best=00000000000000125f03b73dd0dbee093189b152c0a660eb95c406cc884285a0 height=2262933 version=0x20000000 log2_work=79.388307 tx=57646145 date='2025-04-29T10:42:12Z' progress=1.000000 cache=0.5MiB(3198txo) evodb_cache=0.0MiB
      ....
      2025-04-29T10:43:22Z UpdateTip: new best=000000000000003599418914c28f8516a8a44a9fb690861d2aaec47ce5cd7e23 height=2259999 version=0x20000000 log2_work=79.386510 tx=57583502 date='2025-04-24T02:34:58Z' progress=0.998777 cache=5.7MiB(48264txo) evodb_cache=0.0MiB
      ```
    
      ## Breaking Changes
      N/A
    
      ## Checklist:
      - [x] I have performed a self-review of my own code
      - [ ] I have commented my code, particularly in hard-to-understand areas
      - [ ] I have added or updated relevant unit/integration/functional/e2e tests
      - [ ] I have made corresponding changes to the documentation
      - [x] I have assigned this pull request to a milestone
    
    ACKs for top commit:
      UdjinM6:
        ACK df01820fd1bfc23a9e7dcb07ccaa86e8de4bdb03
      PastaPastaPasta:
        utACK df01820fd1bfc23a9e7dcb07ccaa86e8de4bdb03
    
    Tree-SHA512: d2d1bd8120caf488e991d84e3abde136e7b53d0e46dcabad07fb624bc6b146b1e9a1c3bdad71ea258861047efddf3af81419f483c2f335733c7ec3d63a018bf0

--- src/evo/deterministicmns.cpp
+++ src/evo/deterministicmns.cpp
@@ -620,7 +620,6 @@ bool CDeterministicMNManager::UndoBlock(gsl::not_null<const CBlockIndex*> pindex
     int nHeight = pindex->nHeight;
     uint256 blockHash = pindex->GetBlockHash();
 
-    CDeterministicMNList curList;
     CDeterministicMNList prevList;
     CDeterministicMNListDiff diff;
     {
@@ -629,16 +628,16 @@ bool CDeterministicMNManager::UndoBlock(gsl::not_null<const CBlockIndex*> pindex
 
         if (diff.HasChanges()) {
             // need to call this before erasing
-            curList = GetListForBlockInternal(pindex);
             prevList = GetListForBlockInternal(pindex->pprev);
         }
 
         mnListsCache.erase(blockHash);
         mnListDiffsCache.erase(blockHash);
     }
-
     if (diff.HasChanges()) {
-        auto inversedDiff = curList.BuildDiff(prevList);
+        CDeterministicMNList curList{prevList.ApplyDiff(pindex, diff)};
+
+        auto inversedDiff{curList.BuildDiff(prevList)};
         updatesRet = {curList, prevList, inversedDiff};
     }
 
