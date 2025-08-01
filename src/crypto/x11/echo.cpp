de2d18772d18574859ecc4af7b39efa5a8c88667
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-08-01 16:13:04 +0000

    crypto: remove expanded footprint Echo512 impl for small footprint
    This is needed to make the next step easier where we replace generic
    accelerated rounds routines with a specialized `aes_2rounds_all` to
    improve performance, this is harder to do when there are two variants
    to keep track of.

--- src/crypto/x11/echo.cpp
+++ src/crypto/x11/echo.cpp
@@ -35,10 +35,6 @@
 
 #include "sph_echo.h"
 
-#if SPH_SMALL_FOOTPRINT && !defined SPH_SMALL_FOOTPRINT_ECHO
-#define SPH_SMALL_FOOTPRINT_ECHO   1
-#endif
-
 /*
  * We can use a 64-bit implementation only if a 64-bit type is available.
  */
@@ -64,8 +60,6 @@
 				sc->buf + 16 * u + 8); \
 		} \
 	} while (0)
-
-#if SPH_SMALL_FOOTPRINT_ECHO
 
 static void
 aes_2rounds_all(sph_u64 W[16][2],
@@ -105,46 +99,6 @@ aes_2rounds_all(sph_u64 W[16][2],
 		aes_2rounds_all(W, &K0, &K1, &K2, &K3); \
 	} while (0)
 
-#else
-
-#define AES_2ROUNDS(X)   do { \
-		sph_u32 X0 = (sph_u32)(X[0]); \
-		sph_u32 X1 = (sph_u32)(X[0] >> 32); \
-		sph_u32 X2 = (sph_u32)(X[1]); \
-		sph_u32 X3 = (sph_u32)(X[1] >> 32); \
-		sph_u32 Y0, Y1, Y2, Y3; \
-		aes_round_le(X0, X1, X2, X3, K0, K1, K2, K3, Y0, Y1, Y2, Y3); \
-		aes_round_le_nokey(Y0, Y1, Y2, Y3, X0, X1, X2, X3); \
-		X[0] = (sph_u64)X0 | ((sph_u64)X1 << 32); \
-		X[1] = (sph_u64)X2 | ((sph_u64)X3 << 32); \
-		if ((K0 = T32(K0 + 1)) == 0) { \
-			if ((K1 = T32(K1 + 1)) == 0) \
-				if ((K2 = T32(K2 + 1)) == 0) \
-					K3 = T32(K3 + 1); \
-		} \
-	} while (0)
-
-#define BIG_SUB_WORDS   do { \
-		AES_2ROUNDS(W[ 0]); \
-		AES_2ROUNDS(W[ 1]); \
-		AES_2ROUNDS(W[ 2]); \
-		AES_2ROUNDS(W[ 3]); \
-		AES_2ROUNDS(W[ 4]); \
-		AES_2ROUNDS(W[ 5]); \
-		AES_2ROUNDS(W[ 6]); \
-		AES_2ROUNDS(W[ 7]); \
-		AES_2ROUNDS(W[ 8]); \
-		AES_2ROUNDS(W[ 9]); \
-		AES_2ROUNDS(W[10]); \
-		AES_2ROUNDS(W[11]); \
-		AES_2ROUNDS(W[12]); \
-		AES_2ROUNDS(W[13]); \
-		AES_2ROUNDS(W[14]); \
-		AES_2ROUNDS(W[15]); \
-	} while (0)
-
-#endif
-
 #define SHIFT_ROW1(a, b, c, d)   do { \
 		sph_u64 tmp; \
 		tmp = W[a][0]; \
@@ -183,8 +137,6 @@ aes_2rounds_all(sph_u64 W[16][2],
 		SHIFT_ROW3(3, 7, 11, 15); \
 	} while (0)
 
-#if SPH_SMALL_FOOTPRINT_ECHO
-
 static void
 mix_column(sph_u64 W[16][2], int ia, int ib, int ic, int id)
 {
@@ -212,35 +164,6 @@ mix_column(sph_u64 W[16][2], int ia, int ib, int ic, int id)
 }
 
 #define MIX_COLUMN(a, b, c, d)   mix_column(W, a, b, c, d)
-
-#else
-
-#define MIX_COLUMN1(ia, ib, ic, id, n)   do { \
-		sph_u64 a = W[ia][n]; \
-		sph_u64 b = W[ib][n]; \
-		sph_u64 c = W[ic][n]; \
-		sph_u64 d = W[id][n]; \
-		sph_u64 ab = a ^ b; \
-		sph_u64 bc = b ^ c; \
-		sph_u64 cd = c ^ d; \
-		sph_u64 abx = ((ab & C64(0x8080808080808080)) >> 7) * 27U \
-			^ ((ab & C64(0x7F7F7F7F7F7F7F7F)) << 1); \
-		sph_u64 bcx = ((bc & C64(0x8080808080808080)) >> 7) * 27U \
-			^ ((bc & C64(0x7F7F7F7F7F7F7F7F)) << 1); \
-		sph_u64 cdx = ((cd & C64(0x8080808080808080)) >> 7) * 27U \
-			^ ((cd & C64(0x7F7F7F7F7F7F7F7F)) << 1); \
-		W[ia][n] = abx ^ bc ^ d; \
-		W[ib][n] = bcx ^ a ^ cd; \
-		W[ic][n] = cdx ^ ab ^ d; \
-		W[id][n] = abx ^ bcx ^ cdx ^ ab ^ c; \
-	} while (0)
-
-#define MIX_COLUMN(a, b, c, d)   do { \
-		MIX_COLUMN1(a, b, c, d, 0); \
-		MIX_COLUMN1(a, b, c, d, 1); \
-	} while (0)
-
-#endif
 
 #define BIG_MIX_COLUMNS   do { \
 		MIX_COLUMN(0, 1, 2, 3); \
