35d8e8d9b05cb64d5e778ad3760d388246ed38c0
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-08-10 14:14:10 +0000

    rpc: allow `addresses` to report data from legacy platform port fields
    MnNetInfo doesn't actually store this information so we are pulling the
    data from platform{HTTP,P2P}Port to make it appear like it does. This
    is to ensure reporting parity with ExtNetInfo, which stores platform
    fields and should allow us to deprecate the platform{HTTP,P2P}Port when
    we need to.

--- src/rpc/evo_util.h
+++ src/rpc/evo_util.h
@@ -5,12 +5,44 @@
 #ifndef BITCOIN_RPC_EVO_UTIL_H
 #define BITCOIN_RPC_EVO_UTIL_H
 
-class UniValue;
+#include <netaddress.h>
+#include <util/check.h>
+
+#include <evo/dmn_types.h>
+#include <evo/netinfo.h>
+
+#include <cstdint>
+#include <type_traits>
+
+#include <univalue.h>
+
+class CSimplifiedMNListEntry;
 
 template <typename T1>
 void ProcessNetInfoCore(T1& ptx, const UniValue& input, const bool optional);
 
 template <typename T1>
 void ProcessNetInfoPlatform(T1& ptx, const UniValue& input_p2p, const UniValue& input_http, const bool optional);
+
+template <typename T1>
+UniValue ShimNetInfoPlatform(const T1& obj, const MnType& type)
+{
+    UniValue ret{CHECK_NONFATAL(obj.netInfo)->ToJson()};
+
+    // Nothing to do here if not EvoNode, empty or capable of natively storing Platform fields
+    if (type != MnType::Evo || obj.netInfo->IsEmpty() || obj.netInfo->CanStorePlatform()) return ret;
+
+    CNetAddr addr{obj.netInfo->GetPrimary()};
+    ret.pushKV(PurposeToString(Purpose::PLATFORM_HTTPS, /*lower=*/true).data(),
+               ArrFromService(CService(addr, obj.platformHTTPPort)));
+
+    if constexpr (!std::is_same_v<std::decay_t<T1>, CSimplifiedMNListEntry>) {
+        // CSimplifiedMNListEntry doesn't store platformP2PPort, so we cannot report it
+        ret.pushKV(PurposeToString(Purpose::PLATFORM_P2P, /*lower=*/true).data(),
+                   ArrFromService(CService(addr, obj.platformP2PPort)));
+    }
+
+    return ret;
+}
 
 #endif // BITCOIN_RPC_EVO_UTIL_H
