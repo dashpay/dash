1238760a1b644bf34b5e8a4eda4e157fd796e881
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-08-19 19:14:35 +0000

    rpc: allow `addresses` to report data from legacy platform port fields
    MnNetInfo doesn't actually store this information so we are pulling the
    data from platform{HTTP,P2P}Port to make it appear like it does. This
    is to ensure reporting parity with ExtNetInfo, which stores platform
    fields and should allow us to deprecate the platform{HTTP,P2P}Port when
    we need to.

--- src/evo/netinfo.h
+++ src/evo/netinfo.h
@@ -98,6 +98,9 @@ constexpr std::string_view PurposeToString(const NetInfoPurpose purpose)
     return "";
 }
 
+/* Creates a one-element array using CService::ToStringPortAddr() output. */
+UniValue ArrFromService(const CService& addr);
+
 /* Identical to IsDeprecatedRPCEnabled("service"). For use outside of RPC code. */
 bool IsServiceDeprecatedRPCEnabled();
 
