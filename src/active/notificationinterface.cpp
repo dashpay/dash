429e6b615b98087d6490938a59fa2a2715033eb5
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-08-25 11:05:26 +0000

    refactor: move EHF signals handler to `ActiveContext`

--- src/active/notificationinterface.cpp
+++ src/active/notificationinterface.cpp
@@ -4,9 +4,12 @@
 
 #include <active/notificationinterface.h>
 
+#include <active/context.h>
+#include <llmq/ehf_signals.h>
 #include <masternode/node.h>
 
-ActiveNotificationInterface::ActiveNotificationInterface(CActiveMasternodeManager& mn_activeman) :
+ActiveNotificationInterface::ActiveNotificationInterface(ActiveContext& active_ctx, CActiveMasternodeManager& mn_activeman) :
+    m_active_ctx{active_ctx},
     m_mn_activeman{mn_activeman}
 {
 }
@@ -15,6 +18,7 @@ void ActiveNotificationInterface::UpdatedBlockTip(const CBlockIndex* pindexNew, 
                                                   bool fInitialDownload)
 {
     m_mn_activeman.UpdatedBlockTip(pindexNew, pindexFork, fInitialDownload);
+    m_active_ctx.ehf_sighandler->UpdatedBlockTip(pindexNew);
 }
 
 std::unique_ptr<ActiveNotificationInterface> g_active_notification_interface;
