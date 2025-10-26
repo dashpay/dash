bbca2d1957a85e94bdea51de88246670854c628c
pasta <pasta@dashboost.org>
2025-10-26 12:42:35 -0500

    fix: proper include

--- src/test/bls_tests.cpp
+++ src/test/bls_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "span.h"
+#include <span.h>
 #include <bls/bls.h>
 #include <bls/bls_batchverifier.h>
 #include <clientversion.h>
