12938428567ae78297a063c3d418bd37c8aeec93
UdjinM6 <UdjinM6@users.noreply.github.com>
2026-01-19 21:43:01 +0300

    test: update feature_addressindex for async behavior
    Updates the AddressIndex functional test to reflect async operation:
    - AddressIndex can now be enabled/disabled without -reindex
    - Test already includes comprehensive reorg testing that verifies
      proper cleanup of both transaction history and unspent index

--- src/rpc/node.cpp
+++ src/rpc/node.cpp
@@ -4,7 +4,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <index/addressindex.h>
 #include <chainparams.h>
 #include <consensus/consensus.h>
 #include <evo/mnauth.h>
