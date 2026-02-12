a15aa1b5d9404e9028fb9025100e99b3481b1135
Konstantin Akimov <knstqq@gmail.com>
2026-02-12 21:58:11 +0700

    refactor: remove dependency of CInstantSendManager on signer

--- src/instantsend/net_instantsend.h
+++ src/instantsend/net_instantsend.h
@@ -25,6 +25,7 @@ namespace instantsend {
 struct InstantSendLock;
 using InstantSendLockPtr = std::shared_ptr<InstantSendLock>;
 struct PendingISLockEntry;
+class InstantSendSigner;
 } // namespace instantsend
 namespace llmq {
 class CInstantSendManager;
@@ -34,10 +35,12 @@ class CQuorumManager;
 class NetInstantSend final : public NetHandler, public CValidationInterface
 {
 public:
-    NetInstantSend(PeerManagerInternal* peer_manager, llmq::CInstantSendManager& is_manager, llmq::CQuorumManager& qman,
-                   CChainState& chainstate, CTxMemPool& mempool, const CMasternodeSync& mn_sync) :
+    NetInstantSend(PeerManagerInternal* peer_manager, llmq::CInstantSendManager& is_manager,
+                   instantsend::InstantSendSigner* signer, llmq::CQuorumManager& qman, CChainState& chainstate,
+                   CTxMemPool& mempool, const CMasternodeSync& mn_sync) :
         NetHandler(peer_manager),
         m_is_manager{is_manager},
+        m_signer{signer},
         m_qman(qman),
         m_chainstate{chainstate},
         m_mempool{mempool},
@@ -90,7 +93,13 @@ private:
 
     void ResolveBlockConflicts(const uint256& islockHash, const instantsend::InstantSendLock& islock);
 
+    void TruncateRecoveredSigsForInputs(const instantsend::InstantSendLock& islock);
+
+    void HandleFullyConfirmedBlock(const CBlockIndex* pindex);
+    void ClearConflicting(const Uint256HashMap<CTransactionRef>& to_delete);
+
     llmq::CInstantSendManager& m_is_manager;
+    instantsend::InstantSendSigner* m_signer; // non-null only for masternode
     llmq::CQuorumManager& m_qman;
     CChainState& m_chainstate;
     CTxMemPool& m_mempool;
