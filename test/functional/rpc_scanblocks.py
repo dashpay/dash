bitcoin/bitcoin

Merge bitcoin/bitcoin#26307: test: use MiniWallet for rpc_scanblocks.py
ae3626ea52deb22010c0d57d9f992e1e1bc31ba8 test: use MiniWallet for rpc_scanblocks.py (Sebastian Falbesoner)

Pull request description:

  This is a small follow-up for #23549 which introduced `scanblocks`. Since that RPC doesn't need the wallet, we can switch the functional test to use MiniWallet.

ACKs for top commit:
  MarcoFalke:
    review ACK ae3626ea52deb22010c0d57d9f992e1e1bc31ba8

Tree-SHA512: e0b0088103e059b29719299c58fd5173b1cff58cb73025a9a33ad493cd0ac50ba25a5f790e471c00a09b4dca80f3c011174ca4e0c8f34746c39831f5823dc8ba

test/functional/rpc_scanblocks.py
@@ -3,8 +3,16 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test the scanblocks RPC call."""
+from test_framework.messages import COIN
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.util import (
+    assert_equal,
+    assert_raises_rpc_error,
+)
+from test_framework.wallet import (
+    MiniWallet,
+    getnewdestination,
+)
 
 
 class ScanblocksTest(BitcoinTestFramework):
@@ -12,19 +20,21 @@ class ScanblocksTest(BitcoinTestFramework):
         self.num_nodes = 2
         self.extra_args = [["-blockfilterindex=1"], []]
 
-    def skip_test_if_missing_module(self):
-        self.skip_if_no_wallet()
-
     def run_test(self):
         node = self.nodes[0]
+        wallet = MiniWallet(node)
+        wallet.rescan_utxos()
+
         # send 1.0, mempool only
-        addr_1 = node.getnewaddress()
-        node.sendtoaddress(addr_1, 1.0)
+        _, spk_1, addr_1 = getnewdestination()
+        wallet.send_to(from_node=node, scriptPubKey=spk_1, amount=1 * COIN)
 
         parent_key = "tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B"
         # send 1.0, mempool only
         # childkey 5 of `parent_key`
-        node.sendtoaddress("mkS4HXoTYWRTescLGaUTGbtTTYX5EjJyEE", 1.0)
+        wallet.send_to(from_node=node,
+                       scriptPubKey=bytes.fromhex(node.validateaddress("mkS4HXoTYWRTescLGaUTGbtTTYX5EjJyEE")['scriptPubKey']),
+                       amount=1 * COIN)
 
         # mine a block and assure that the mined blockhash is in the filterresult
         blockhash = self.generate(node, 1)[0]

dashpay/dash

test/functional/rpc_scanblocks.py
@@ -3,31 +3,38 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test the scanblocks RPC call."""
+from test_framework.messages import COIN
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.util import (
+    assert_equal,
+    assert_raises_rpc_error,
+)
+from test_framework.wallet import (
+    MiniWallet,
+    getnewdestination,
+)
 
 
 class ScanblocksTest(BitcoinTestFramework):
-    def add_options(self, parser):
-        self.add_wallet_options(parser, legacy=False)
-
     def set_test_params(self):
         self.num_nodes = 2
         self.extra_args = [["-blockfilterindex=1"], []]
 
-    def skip_test_if_missing_module(self):
-        self.skip_if_no_wallet()
-
     def run_test(self):
         node = self.nodes[0]
+        wallet = MiniWallet(node)
+        wallet.rescan_utxos()
+
         # send 1.0, mempool only
-        addr_1 = node.getnewaddress()
-        node.sendtoaddress(addr_1, 1.0)
+        _, spk_1, addr_1 = getnewdestination()
+        wallet.send_to(from_node=node, scriptPubKey=spk_1, amount=1 * COIN)
 
         parent_key = "tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B"
         # send 1.0, mempool only
         # childkey 5 of `parent_key`
-        node.sendtoaddress("yREYqgSp8jrsNSeqyk8hLEnGiAzRwM6dKp", 1.0)
+        wallet.send_to(from_node=node,
+                       scriptPubKey=bytes.fromhex(node.validateaddress("yREYqgSp8jrsNSeqyk8hLEnGiAzRwM6dKp")['scriptPubKey']),
+                       amount=1 * COIN)
 
         # mine a block and assure that the mined blockhash is in the filterresult
         blockhash = self.generate(node, 1)[0]