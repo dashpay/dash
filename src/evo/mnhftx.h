d1cd2121fab0984d1c38d2c31bde543cf08a7832
pasta <pasta@dashboost.org>
2026-03-09 18:07:23 -0500

    ci: select Blacksmith runners dynamically

--- src/evo/mnhftx.h
+++ src/evo/mnhftx.h
@@ -89,7 +89,7 @@ class CMNHFManager : public AbstractEHFManager
 {
 private:
     CEvoDB& m_evoDb;
-    // TODO: move its functionallity of ProcessBlock, UndoBlock to specialtxman;
+    // TODO: move its functionality of ProcessBlock, UndoBlock to specialtxman;
     // it will help to drop dependency on m_chainman, m_qman here (and validation.h)
     // Secondly, store in database active EHF signals not for each block;
     // but quite opposite: keep only hash of block where signal is added.
